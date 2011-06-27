f5ba46680f96137096fd30e09de867f977cae76c
samaursa <samaursa@localhost>
2011-06-27 11:30:14 -0400

    [ADD][TwoLocDep 1eed3473ef44] - Added file structure for containers (empty)

--- src/tlocCore/tlocString.h
+++ src/tlocCore/tlocString.h
@@ -1,1 +1,27 @@
-// tlocString header
+#ifndef TLOC_STRING_H
+#define TLOC_STRING_H
+
+namespace tloc
+{
+  template <typename T>
+  class String
+  {
+  public:
+    String()
+    {
+
+    }
+
+    ~String()
+    {
+
+    }
+
+
+
+  private:
+    T* m_string;
+  };
+};
+
+#endif

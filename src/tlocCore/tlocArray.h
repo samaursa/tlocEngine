5d8b381323ee4d206e496ed772c4772ff8bf13c2
samaursa <samaursa@localhost>
2011-07-01 09:48:39 -0400

    [INCR][ADD][TwoLocDep 1eed3473ef44] Syncronization commit | Added Doug Lea Memory allocator

---
+++ src/tlocCore/tlocArray.h
@@ -0,0 +1,19 @@
+#ifndef TLOC_ARRAY_H
+#define TLOC_ARRAY_H
+
+namespace tloc
+{
+  template <typename T>
+  class Array
+  {
+  public:
+
+
+  private:
+    T*    m_data;
+    u32   m_size;
+
+  };
+};
+
+#endif

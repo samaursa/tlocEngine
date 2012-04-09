c92bfa1fbaa4530878a1c2de0d9ddc2bb4d21d29
samaursa <samaursa@localhost>
2012-04-09 12:38:19 -0400

    [WIP][FIX] Fixed errors with version tool in some configurations

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_61_1333988115
+#ifndef TLOC_VERSION_0_0_77_1333989414
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

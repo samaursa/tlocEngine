3d5955cd041934da8a78feff5aa86b3ab40665ac
samaursa <samaursa@localhost>
2012-05-13 02:19:49 -0400

    [OBJ][FIX][REF] Fixed x64 compilation issues across the board (tested) | Refactored Tuple<> and Table<> to conform to our coding standards - x64         * the x64 bit issues were addressed by changing all types to generic         * most iteration loops now use tl_int instead of tl_size because the []           operator uses tl_int (using tl_size causes a mismatch warning) - tlocTuple/tlocTable         * Added typedefs for various types         * Added macros for template declarations (making the classes cleaner)

--- src/tlocCore/tlocTable.h
+++ src/tlocCore/tlocTable.h
@@ -14,11 +14,12 @@ namespace tloc { namespace core {
   // may be faster. These include Setting the columns via a Tuple<> and
   // getting a column via a Tuple<>. Vice-versa is true for row major order
   // (not yet supported).
-  template <typename T, u32 T_Rows, u32 T_Cols>
+  template <typename T, tl_size T_Rows, tl_size T_Cols>
   class Table
   {
   protected:
-    enum { k_TableSize = T_Rows * T_Cols };
+    enum { k_Rows = T_Rows, k_Cols = T_Cols };
+    enum { k_TableSize = k_Rows * k_Cols };
     T m_values[k_TableSize];
 
   public:
@@ -29,25 +30,31 @@ namespace tloc { namespace core {
     // of values
     typedef enum TABLE_ORDER { k_RowMajor, k_ColMajor } TABLE_ORDER;
 
-    TL_FI Table();
-    TL_FI Table(const Table<T, T_Rows, T_Cols>& aTable);
+    typedef T                                           value_type;
+    typedef tl_size                                     size_type;
+    typedef Table<value_type, k_Rows, k_Cols>           this_type;
+    typedef Tuple<value_type, k_Rows>                   tuple_row_type;
+    typedef Tuple<value_type, k_Cols>                   tuple_col_type;
 
-    TL_FI explicit Table(const T& aValue);
-    TL_FI Table(const T (&values)[k_TableSize], TABLE_ORDER aTableOrder);
+    TL_FI Table();
+    TL_FI Table(const this_type& aTable);
+
+    TL_FI explicit Table(const value_type& aValue);
+    TL_FI Table(const value_type (&values)[k_TableSize], TABLE_ORDER aTableOrder);
 
     //------------------------------------------------------------------------
     // Accessors
 
     // Access range is that of an integer, approx. 2.1 billion rows & cols.
     // If size is more than that, use the get function instead
-    TL_FI T& operator() (s32 aRow, s32 aCol);
-    TL_FI const T& operator() (s32 aRow, s32 aCol) const;
+    TL_FI value_type& operator() (tl_int aRow, tl_int aCol);
+    TL_FI const value_type& operator() (tl_int aRow, tl_int aCol) const;
 
-    TL_FI T& Get(u32 aRow, u32 aCol);
-    TL_FI const T& Get(u32 aRow, u32 aCol) const;
+    TL_FI value_type& Get(size_type aRow, size_type aCol);
+    TL_FI const value_type& Get(size_type aRow, size_type aCol) const;
 
-    TL_FI void GetRow(u32 aRow, Tuple<T, T_Cols>& aRowOut) const;
-    TL_FI void GetCol(u32 aCol, Tuple<T, T_Rows>& aColOut) const;
+    TL_FI void GetRow(size_type aRow, tuple_col_type& aRowOut) const;
+    TL_FI void GetCol(size_type aCol, tuple_row_type& aColOut) const;
 
     // Direct array access. Generally not recommended but useful for memcpy
     TL_FI operator T* ();
@@ -56,16 +63,17 @@ namespace tloc { namespace core {
     //------------------------------------------------------------------------
     // Modifiers
 
-    TL_FI void Set(const T& aValue);
-    TL_FI void Set(const T (&values)[k_TableSize], TABLE_ORDER aTableOrder);
-    TL_FI void Set(u32 aRow, u32 aCol, const T& aValue);
-    TL_FI void SetRow(u32 aRow, const Tuple<T, T_Cols>& aRowIn);
-    TL_FI void SetCol(u32 aCol, const Tuple<T, T_Rows>& aColIn);
+    TL_FI void Set(const value_type& aValue);
+    TL_FI void Set(const value_type (&values)[k_TableSize],
+                   TABLE_ORDER aTableOrder);
+    TL_FI void Set(size_type aRow, size_type aCol, const T& aValue);
+    TL_FI void SetRow(size_type aRow, const tuple_col_type& aRowIn);
+    TL_FI void SetCol(size_type aCol, const tuple_row_type& aColIn);
 
     //------------------------------------------------------------------------
     // Operators
 
-    TL_FI Table<T, T_Rows, T_Cols>& operator = (const Table<T, T_Rows, T_Cols>& aTable);
+    TL_FI this_type& operator = (const this_type& aTable);
     TL_FI bool operator == (const Table& aTable);
     TL_FI bool operator != (const Table& aTable);
   };
--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_1013_1336885047
+#ifndef TLOC_VERSION_0_0_1013_1336889678
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

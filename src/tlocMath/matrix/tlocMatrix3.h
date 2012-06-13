861799e36b779283768f0e279af1708cf2a9d0e5
Matiaus <Matiaus@localhost>
2012-06-13 14:31:25 -0400

    [REF] Refactored the code in tlocMath to use "using" declarations when referring to base class member functions (tested on windows).

--- src/tlocMath/matrix/tlocMatrix3.h
+++ src/tlocMath/matrix/tlocMatrix3.h
@@ -23,8 +23,10 @@ namespace tloc { namespace math {
     
     //------------------------------------------------------------------------
     // using declarations for access to base class
-    using base_type::k_MatrixSize;
+    using base_type::Set;
+    using base_type::MakeDiagonal;
     using base_type::m_values;
+    using base_type::k_MatrixSize;
     
     //------------------------------------------------------------------------
     // Constructors
--- src/tlocMath/vector/tlocVector3.h
+++ src/tlocMath/vector/tlocVector3.h
@@ -28,6 +28,8 @@ namespace tloc { namespace math {
     
     //------------------------------------------------------------------------
     // using declarations for access to base class
+    using base_type::operator=;
+    using base_type::LengthSquared;
     using base_type::m_values;
     
     //------------------------------------------------------------------------

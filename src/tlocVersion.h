8fb134c286f0d8906fffce6feb408269b60b2cfa
Matiaus <Matiaus@localhost>
2012-08-09 04:58:04 -0400

    [B-FIX][MIN] Fixed minor syntax errors on Mac build for string (tested)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_2521_1342911765
+#ifndef TLOC_VERSION_0_0_2525_1344502422
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

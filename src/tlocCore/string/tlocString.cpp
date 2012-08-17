aaae25f1e96ded35680e61b00b14579e26aa7e96
Matiaus <Matiaus@localhost>
2012-08-17 18:02:12 -0400

    [ADD][REF]Added Xcode project for tlocCore|Separted platform defines into PlatformDefines.h|Allowed tlocString to be explicitly instantiated

--- src/tlocCore/string/tlocString.cpp
+++ src/tlocCore/string/tlocString.cpp
@@ -1,11 +1,11 @@
 #include "tlocString.h"
 #include "tlocString.inl"
 
-//namespace tloc { namespace core {
-//
-//  template class StringBase<char8>;
-//  template class StringBase<char32>;
-//
-//};};
+namespace tloc { namespace core {
 
-TLOC_INTENTIONALLY_EMPTY_SOURCE_FILE();
+  template class StringBase<char8>;
+  template class StringBase<char32>;
+
+};};
+
+//TLOC_INTENTIONALLY_EMPTY_SOURCE_FILE();

c56a54ae9ace813ff104e4f94515372185173f47
samaursa <samaursa@localhost>
2012-09-26 16:56:16 -0400

    [OBJ] Completed math::Angle (tested) - tlocAngle         * Minimized duplicate code as much as possible         * Theoretically, the compiler should compile out all the function calls           that get resolved at compile time ==> In other words, this class           should be very fast with all the type-safety that other's (e.g. Ogre)           do not offer

--- src/tlocMath/tlocAngle.cpp
+++ src/tlocMath/tlocAngle.cpp
@@ -12,48 +12,54 @@ namespace tloc { namespace math {
   //////////////////////////////////////////////////////////////////////////
   // Explicit instantiations
 
-  //------------------------------------------------------------------------
-  // Angle
+#include "tlocAngleExplicitMacros.h"
+  TLOC_EXPLICITLY_INSTANTIATE_ANGLE(f32, Radian_T, Degree_T);
+  TLOC_EXPLICITLY_INSTANTIATE_ANGLE(f64, Radian_T, Degree_T);
+  TLOC_EXPLICITLY_INSTANTIATE_ANGLE(f32, Degree_T, Radian_T);
+  TLOC_EXPLICITLY_INSTANTIATE_ANGLE(f64, Degree_T, Radian_T);
 
-  template class Angle_T<f32, Radian_T<f32> >;
-  template class Angle_T<f64, Radian_T<f64> >;
+  ////------------------------------------------------------------------------
+  //// Angle
 
-  template f32 Angle_T<f32, Radian_T<f32> >::GetAs<Radian_T<f32> >() const;
-  template f32 Angle_T<f32, Radian_T<f32> >::GetAs<Degree_T<f32> >() const;
-  template f64 Angle_T<f64, Radian_T<f64> >::GetAs<Radian_T<f64> >() const;
-  template f64 Angle_T<f64, Radian_T<f64> >::GetAs<Degree_T<f64> >() const;
+  //template class Angle_T<f32, Radian_T<f32> >;
+  //template class Angle_T<f64, Radian_T<f64> >;
 
-  template f32 Angle_T<f32, Degree_T<f32> >::GetAs<Radian_T<f32> >() const;
-  template f32 Angle_T<f32, Degree_T<f32> >::GetAs<Degree_T<f32> >() const;
-  template f64 Angle_T<f64, Degree_T<f64> >::GetAs<Radian_T<f64> >() const;
-  template f64 Angle_T<f64, Degree_T<f64> >::GetAs<Degree_T<f64> >() const;
+  //template f32 Angle_T<f32, Radian_T<f32> >::GetAs<Radian_T<f32> >() const;
+  //template f32 Angle_T<f32, Radian_T<f32> >::GetAs<Degree_T<f32> >() const;
+  //template f64 Angle_T<f64, Radian_T<f64> >::GetAs<Radian_T<f64> >() const;
+  //template f64 Angle_T<f64, Radian_T<f64> >::GetAs<Degree_T<f64> >() const;
 
-  //------------------------------------------------------------------------
-  // Radian
+  //template f32 Angle_T<f32, Degree_T<f32> >::GetAs<Radian_T<f32> >() const;
+  //template f32 Angle_T<f32, Degree_T<f32> >::GetAs<Degree_T<f32> >() const;
+  //template f64 Angle_T<f64, Degree_T<f64> >::GetAs<Radian_T<f64> >() const;
+  //template f64 Angle_T<f64, Degree_T<f64> >::GetAs<Degree_T<f64> >() const;
 
-  template Radian_T<f32>::Radian_T(Radian_T<f32>);
-  template Radian_T<f32>::Radian_T(Degree_T<f32>);
-  template Radian_T<f64>::Radian_T(Radian_T<f64>);
-  template Radian_T<f64>::Radian_T(Degree_T<f64>);
+  ////------------------------------------------------------------------------
+  //// Radian
 
-  template void Radian_T<f32>::DoSetAngle(Degree_T<f32>);
-  template void Radian_T<f64>::DoSetAngle(Degree_T<f32>);
+  //template Radian_T<f32>::Radian_T(Radian_T<f32>);
+  //template Radian_T<f32>::Radian_T(Degree_T<f32>);
+  //template Radian_T<f64>::Radian_T(Radian_T<f64>);
+  //template Radian_T<f64>::Radian_T(Degree_T<f64>);
 
-  template Radian_T<f32>::value_type Radian_T<f32>::DoGetAs(Degree_T<f32>) const;
-  template Radian_T<f64>::value_type Radian_T<f64>::DoGetAs(Degree_T<f64>) const;
+  //template void Radian_T<f32>::DoSetAngle(Degree_T<f32>);
+  //template void Radian_T<f64>::DoSetAngle(Degree_T<f32>);
 
-  //------------------------------------------------------------------------
-  // Degree
+  //template Radian_T<f32>::value_type Radian_T<f32>::DoGetAs(Degree_T<f32>) const;
+  //template Radian_T<f64>::value_type Radian_T<f64>::DoGetAs(Degree_T<f64>) const;
 
-  template Degree_T<f32>::Degree_T(Radian_T<f32>);
-  template Degree_T<f32>::Degree_T(Degree_T<f32>);
-  template Degree_T<f64>::Degree_T(Radian_T<f64>);
-  template Degree_T<f64>::Degree_T(Degree_T<f64>);
+  ////------------------------------------------------------------------------
+  //// Degree
 
-  template void Degree_T<f64>::DoSetAngle(Degree_T<f32>);
-  template void Degree_T<f64>::DoSetAngle(Degree_T<f32>);
+  //template Degree_T<f32>::Degree_T(Radian_T<f32>);
+  //template Degree_T<f32>::Degree_T(Degree_T<f32>);
+  //template Degree_T<f64>::Degree_T(Radian_T<f64>);
+  //template Degree_T<f64>::Degree_T(Degree_T<f64>);
 
-  template Degree_T<f32>::value_type Degree_T<f32>::DoGetAs(Degree_T<f32>) const;
-  template Degree_T<f64>::value_type Degree_T<f64>::DoGetAs(Degree_T<f64>) const;
+  //template void Degree_T<f64>::DoSetAngle(Degree_T<f32>);
+  //template void Degree_T<f64>::DoSetAngle(Degree_T<f32>);
+
+  //template Degree_T<f32>::value_type Degree_T<f32>::DoGetAs(Degree_T<f32>) const;
+  //template Degree_T<f64>::value_type Degree_T<f64>::DoGetAs(Degree_T<f64>) const;
 
 };};

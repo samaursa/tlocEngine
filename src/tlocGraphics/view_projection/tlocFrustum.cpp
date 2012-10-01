03e1d22bbb5dcd8cf1b0ea9607525c9c0b57d2c4
samaursa <samaursa@localhost>
2012-09-30 23:41:51 -0400

    [WIP] Working on Frustum

--- src/tlocGraphics/view_projection/tlocFrustum.cpp
+++ src/tlocGraphics/view_projection/tlocFrustum.cpp
@@ -1,4 +1,5 @@
 #include "tlocFrustum.h"
+#include <tlocCore/data_structures/tlocTuple.inl>
 
 namespace tloc { namespace graphics { namespace view_projection {
 
@@ -7,5 +8,10 @@ namespace tloc { namespace graphics { namespace view_projection {
 
   Frustum::~Frustum()
   { }
+
+  //------------------------------------------------------------------------
+  // Instantiate tuple for the number of planes we have
+
+  template class core::Tuple<Frustum::real_type, Frustum::Planes::k_count>;
 
 };};};
--- src/tlocGraphics/view_projection/tlocFrustum.h
+++ src/tlocGraphics/view_projection/tlocFrustum.h
@@ -1,13 +1,43 @@
 #ifndef _TLOC_GRAPHICS_VIEW_PROJECTION_FRUSTUM_H
 #define _TLOC_GRAPHICS_VIEW_PROJECTION_FRUSTUM_H
 
+#include <tlocCore/data_structures/tlocTuple.h>
+
 namespace tloc { namespace graphics { namespace view_projection {
 
   class Frustum
   {
   public:
+    struct Planes
+    {
+      enum
+      {
+        k_near = 0,
+        k_far,
+        k_top,
+        k_bottom,
+        k_left,
+        k_right,
+        k_count,
+      };
+
+    private:
+      Planes();
+    };
+
+  public:
+    typedef tl_float                                    real_type;
+    typedef core::Tuple<real_type, Planes::k_count>     cont_type;
+    typedef tl_size                                     size_type;
+
+  public:
     Frustum();
     ~Frustum();
+
+    void BuildFrustum();
+
+  private:
+    cont_type m_planes;
   };
 
 };};};

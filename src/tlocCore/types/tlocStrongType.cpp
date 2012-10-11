0fcc3518689ca0f990272ec9525d2436a96bec4d
samaursa <samaursa@localhost>
2012-10-10 20:21:18 -0400

    [MIN] Moved macro for explicity instantiation of Strong Type to a separate file - This will allow other classes to easily instantiate the type if required

--- src/tlocCore/types/tlocStrongType.cpp
+++ src/tlocCore/types/tlocStrongType.cpp
@@ -3,17 +3,7 @@
 
 namespace tloc { namespace core { namespace types {
 
-#define TLOC_INSTANTIATE_STRONG_TYPE(_type_)\
-  template struct StrongType_T<_type_, 0>;\
-  template struct StrongType_T<_type_, 1>;\
-  template struct StrongType_T<_type_, 2>;\
-  template struct StrongType_T<_type_, 3>;\
-  template struct StrongType_T<_type_, 4>;\
-  template struct StrongType_T<_type_, 5>;\
-  template struct StrongType_T<_type_, 6>;\
-  template struct StrongType_T<_type_, 7>;\
-  template struct StrongType_T<_type_, 8>;\
-  template struct StrongType_T<_type_, 9>
+#include "tlocStrongTypeExplicitMacros.h"
 
   TLOC_INSTANTIATE_STRONG_TYPE(s8);
   TLOC_INSTANTIATE_STRONG_TYPE(u8);

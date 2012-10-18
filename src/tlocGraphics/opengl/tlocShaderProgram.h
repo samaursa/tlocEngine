9c326d52f5cdc2b37b4f9c91e634105b5d772aab
samaursa <samaursa@localhost>
2012-10-18 17:10:00 -0400

    [ADD][MOD] Added Uniform class (will be followed by Attribute class) | Typesafe glGetProgramIv alternative added to ShaderProgram | Extra functionality added for info queries (TESTED) - tlocShaderProgram         * glGetProgramIv is wrapped in GetInfo() function which is typesafe         * Can now query number of attributes and uniforms in the linked program

--- src/tlocGraphics/opengl/tlocShaderProgram.h
+++ src/tlocGraphics/opengl/tlocShaderProgram.h
@@ -9,6 +9,19 @@
 
 namespace tloc { namespace graphics { namespace gl {
 
+  namespace p_shader_program
+  {
+    struct DeleteStatus{ static const tl_int s_glStatusName; };
+    struct LinkStatus{ static const tl_int s_glStatusName; };
+    struct ValidateStatus{ static const tl_int s_glStatusName; };
+    struct InfoLogLength{ static const tl_int s_glStatusName; };
+    struct AttachedShaders{ static const tl_int s_glStatusName; };
+    struct ActiveAttributes{ static const tl_int s_glStatusName; };
+    struct ActiveAttributeMaxLength{ static const tl_int s_glStatusName; };
+    struct ActiveUniforms{ static const tl_int s_glStatusName; };
+    struct ActiveUniformMaxLength{ static const tl_int s_glStatusName; };
+  };
+
   class ShaderProgram : public Object_T<ShaderProgram>
   {
   public:
@@ -22,9 +35,11 @@ namespace tloc { namespace graphics { namespace gl {
     typedef core::Variadic<Shader*, 4>     four_shader_components;
 
     typedef Object_T<ShaderProgram>     base_type;
-    using base_type::object_handle;
+    typedef base_type::object_handle    object_handle;
+    typedef base_type::error_type       error_type;
 
     typedef tl_size                     size_type;
+    typedef s32                         gl_result_type;
 
   public:
     ShaderProgram();
@@ -32,12 +47,35 @@ namespace tloc { namespace graphics { namespace gl {
     template <size_type T_Size>
     bool AttachShaders(core::Variadic<Shader*, T_Size>
                        a_shaderComponents);
-
     bool Link();
+
+    template <typename T_ProgramIvParam>
+    gl_result_type GetInfo() const
+    {
+      type_traits::AssertTypeIsSupported
+        < T_ProgramIvParam,
+        p_shader_program::DeleteStatus,
+        p_shader_program::LinkStatus,
+        p_shader_program::ValidateStatus,
+        p_shader_program::InfoLogLength,
+        p_shader_program::AttachedShaders,
+        p_shader_program::ActiveAttributes,
+        p_shader_program::ActiveAttributeMaxLength,
+        p_shader_program::ActiveUniforms,
+        p_shader_program::ActiveUniformMaxLength>();
+      return DoGetInfo<T_ProgramIvParam>();
+    }
+
+    size_type GetNumAttributes() const;
+    size_type GetNumUniforms() const;
+
     void Enable();
 
   private:
     void DoDestroy();
+
+    template <typename T_ProgramIvParam>
+    gl_result_type DoGetInfo() const;
 
   private:
     core::utils::Checkpoints    m_flags;

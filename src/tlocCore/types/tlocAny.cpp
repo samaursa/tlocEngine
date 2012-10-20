3bec001fa8aee985e4efe2fbbab25ffd5556bc17
samaursa <samaursa@localhost>
2012-10-20 19:29:55 -0400

    [INCR][ADD] Added tlocAny (UNTESTED) - tlocAny (UNTESTED)         * Provides compile-time type-safety where possible and full run-time           type safety otherwise         * For use in scenarios where a member variable must be able to store           several different types (useful scenario is for OpenGL attributes and           uniforms where our wrapper classes will use Any() to store any           attribute/uniform with compile-time and run-time type safety

---
+++ src/tlocCore/types/tlocAny.cpp
@@ -0,0 +1,62 @@
+#include "tlocAny.h"
+
+#include <tlocCore/tlocAlgorithms.h>
+#include <tlocCore/tlocAlgorithms.inl>
+
+namespace tloc { namespace core { namespace types {
+
+  Any::Any()  : m_policy(p_any::GetPolicy<p_any::detail::Empty>())
+              , m_object(NULL)
+  { }
+
+  Any::Any( const char* a_other )
+    : m_policy(p_any::GetPolicy<p_any::detail::Empty>())
+    , m_object(NULL)
+  {
+    Assign(a_other);
+  }
+
+  Any::Any( const this_type& a_other )
+    : m_policy(p_any::GetPolicy<p_any::detail::Empty>())
+    , m_object(NULL)
+  {
+    Assign(a_other);
+  }
+
+  Any::~Any()
+  {
+    m_policy->Delete(&m_object);
+  }
+
+  void Any::Assign( const this_type& a_other )
+  {
+    Reset();
+    m_policy = a_other.m_policy;
+    m_policy->Clone(&a_other.m_object, &m_object);
+  }
+
+  Any::this_type&
+    Any::Swap( this_type& a_other )
+  {
+    tlSwap(m_policy, a_other.m_policy);
+    tlSwap(m_object, a_other.m_object);
+    return *this;
+  }
+
+  bool Any::IsEmpty() const
+  {
+    return m_policy == p_any::GetPolicy<p_any::detail::Empty>();
+  }
+
+  void Any::Reset()
+  {
+    m_policy->Delete(&m_object);
+    m_policy = p_any::GetPolicy<p_any::detail::Empty>();
+  }
+
+  bool Any::IsSame( const this_type& a_other ) const
+  {
+    return m_policy == a_other.m_policy;
+  }
+
+};};};

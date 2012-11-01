ab98ce45b189fc6c9d2ecac67f501f6ceeca3acc
samaursa <samaursa@localhost>
2012-11-01 13:12:02 -0400

    [MIN] Tested to work with the latest dependency revision

--- src/tlocCore/tlocDepVersionCheck.cpp
+++ src/tlocCore/tlocDepVersionCheck.cpp
@@ -1,5 +1,5 @@
 // Compile against the proper dependency version
-#define TLOC_DEP_VERSION_0_0_38_1348245030
+#define TLOC_DEP_VERSION_0_0_39_1351789718
 #include "tlocDepVersion.h"
 
 #include <tlocCore/tlocBase.h>

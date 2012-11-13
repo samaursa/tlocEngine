ff8a8127bcbab4d1dfa357bb754b67f7ee41cb53
Matiaus <Matiaus@localhost>
2012-11-13 01:51:30 -0500

    [WIP][MOD][REF]tlocPhysicsManager now returns an error for initialize and added asserts to make sure the World is allocated

--- src/tlocPhysics/tlocBox2D/tlocPhysicsManager.cpp
+++ src/tlocPhysics/tlocBox2D/tlocPhysicsManager.cpp
@@ -6,8 +6,16 @@
 
 namespace tloc { namespace physics { namespace box2d {
 
+  enum flags
+  {
+    // CHANGE FLAG TYPE
+    initialized,
+    count
+  };
+
   PhysicsManager::PhysicsManager()
-    : m_world(NULL)
+    : m_flags(count)
+    , m_world(NULL)
   {
   }
 
@@ -16,21 +24,43 @@ namespace tloc { namespace physics { namespace box2d {
     delete m_world;
   }
 
-  void PhysicsManager::Initialize(gravity a_gravity, 
-                                  velocity_iterations a_velocityIterations, 
-                                  position_iterations a_positionIterations)
+  PhysicsManager::error_type 
+    PhysicsManager::Initialize(gravity a_gravity, 
+                               velocity_iterations a_velocityIterations, 
+                               position_iterations a_positionIterations)
   {
-    TLOC_ASSERT(m_world, "PhysicsManager has already been initialized!");
+    TLOC_ASSERT(!m_flags[initialized], 
+                "PhysicsManager has already been initialized!");
 
     m_world = new World(a_gravity);
     m_velocityIterations = a_velocityIterations;
     m_positionIterations = a_positionIterations;
+    
+    m_flags.Mark(initialized);
+    return ErrorSuccess();
   }
 
   void PhysicsManager::Update(tl_float a_timeStep)
   {
+    TLOC_ASSERT(m_flags[initialized], "PhysicsManager is not initialized!");
+    TLOC_ASSERT_NOT_NULL(m_world);
+
     m_world->GetWorld().Step(a_timeStep, 
                              m_velocityIterations, m_positionIterations);
+  }
+
+  PhysicsManager::world_type& PhysicsManager::GetWorld()
+  {
+    TLOC_ASSERT(m_flags[initialized], "PhysicsManager is not initialized!");
+    TLOC_ASSERT_NOT_NULL(m_world);
+    return *m_world;
+  }
+
+  const PhysicsManager::world_type& PhysicsManager::GetWorld() const
+  {
+    TLOC_ASSERT(m_flags[initialized], "PhysicsManager is not initialized!");
+    TLOC_ASSERT_NOT_NULL(m_world);
+    return *m_world;
   }
 
 };};};
--- src/tlocPhysics/tlocBox2D/tlocPhysicsManager.h
+++ src/tlocPhysics/tlocBox2D/tlocPhysicsManager.h
@@ -5,6 +5,8 @@
 #include <tlocCore/types/tlocTypes.h>
 #include <tloccore/types/tlocStrongType.h>
 #include <tlocCore/utilities/tlocUtils.h>
+#include <tloccore/error/tlocError.h>
+#include <tloccore/utilities/tlocCheckpoints.h>
 
 #include <tlocMath/vector/tlocVector2.h>
 
@@ -15,9 +17,11 @@ namespace tloc { namespace physics { namespace box2d {
   class PhysicsManager
   {
   public:
-    typedef PhysicsManager  this_type;
-    typedef s32             int_type;
-    typedef math::Vec2f     vec_type;
+    typedef PhysicsManager      this_type;
+    typedef s32                 int_type;
+    typedef math::Vec2f         vec_type;
+    typedef core::error::Error  error_type;
+    typedef World               world_type;
 
     typedef core::types::StrongType_T<int_type, 0> velocity_iterations;
     typedef core::types::StrongType_T<int_type, 1> position_iterations;
@@ -28,13 +32,14 @@ namespace tloc { namespace physics { namespace box2d {
     PhysicsManager();
     ~PhysicsManager();
 
-    void Initialize(gravity a_gravity, 
-                    velocity_iterations a_vel = velocity_iterations(8), 
-                    position_iterations a_pos = position_iterations(3));
+    error_type Initialize(gravity a_gravity, 
+                          velocity_iterations a_vel = velocity_iterations(8), 
+                          position_iterations a_pos = position_iterations(3));
 
     void Update(tl_float a_timeStep);
 
-    TLOC_DECL_AND_DEF_GETTERS_DIRECT(World, GetWorld, *m_world);
+    world_type& GetWorld();
+    const world_type& GetWorld() const;
 
     TLOC_DECL_AND_DEF_GETTER(int_type, GetVelocityIterations, 
                              m_velocityIterations);
@@ -47,7 +52,8 @@ namespace tloc { namespace physics { namespace box2d {
                              m_positionIterations);
 
   private:
-    World* m_world;
+    core::utils::Checkpoints m_flags;
+    world_type* m_world;
     int_type m_velocityIterations;
     int_type m_positionIterations;
   };

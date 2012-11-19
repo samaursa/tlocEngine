8d57e43d6a7eea5f7ec68aa8e292d7a5f0054a75
samaursa <samaursa@localhost>
2012-11-18 21:54:37 -0500

    [FIX][MOD] Fixed linking errors when passing arrays to a gl::Variable | Modified the way types are checked against supported types - tlocShaderVariable         * The derived class must now implement three functions for type           checking which is essentially a breakdown of the original type           checking function

--- src/tlocGraphics/opengl/tlocUniform.h
+++ src/tlocGraphics/opengl/tlocUniform.h
@@ -49,6 +49,41 @@ namespace tloc { namespace graphics { namespace gl {
         >();
     }
 
+    template <typename T>
+    void DoCheckNonArrayTypes() const
+    {
+      type_traits::AssertTypeIsSupported
+        <T,
+         f32,
+         math::Vec2f32,
+         math::Vec3f32,
+         math::Vec4f32,
+         s32,
+         core::Tuple2s32,
+         core::Tuple3s32,
+         core::Tuple4s32,
+         math::Mat2f32,
+         math::Mat3f32,
+         math::Mat4f32
+        >();
+    }
+
+    template <typename T>
+    void DoCheckArrayTypes() const
+    {
+      type_traits::AssertTypeIsSupported
+        <core::Array<T>,
+         core::Array<f32>,
+         core::Array<math::Vec2f32>,
+         core::Array<math::Vec3f32>,
+         core::Array<math::Vec4f32>,
+         core::Array<s32>,
+         core::Array<core::Tuple2s32>,
+         core::Array<core::Tuple3s32>,
+         core::Array<core::Tuple4s32>
+        >();
+    }
+
   };
 
 };};};

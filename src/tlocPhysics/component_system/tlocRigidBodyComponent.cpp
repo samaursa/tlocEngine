085a6f90d0778ab7131a784f3172ef61ab04d663
Matiaus <Matiaus@localhost>
2012-11-23 18:22:54 -0500

    [INCR][MOD] Modified tlocRigidBodyComponent to work with tlocRigidBody

--- src/tlocPhysics/component_system/tlocRigidBodyComponent.cpp
+++ src/tlocPhysics/component_system/tlocRigidBodyComponent.cpp
@@ -6,28 +6,8 @@ namespace tloc { namespace physics { namespace component_system{
 
   RigidBody::RigidBody(const rigid_body_def_type& a_box2dRigidBodyDef)
     : base_type(components::k_rigid_body)
-    , m_box2dRigidBodyDef(a_box2dRigidBodyDef)
-    , m_box2dRigidBody(NULL)
+    , m_rigidBodyDef(a_box2dRigidBodyDef)
   {
-  }
-
-  RigidBody::rigid_body_type* RigidBody::GetBox2dRigidBody()
-  {
-    return m_box2dRigidBody;
-  }
-
-  void RigidBody::SetBox2dRigidBody(rigid_body_type* a_box2dRigidBody)
-  {
-    TLOC_ASSERT(!a_box2dRigidBody,
-                "Internal rigid body cannot be set to null! \
-                Use SetBox2dRigidBodyToNull");
-
-    m_box2dRigidBody = a_box2dRigidBody;
-  }
-
-  void RigidBody::SetBox2dRigidBodyToNull()
-  {
-    m_box2dRigidBody = NULL;
   }
 
 };};};
--- src/tlocPhysics/component_system/tlocRigidBodyComponent.h
+++ src/tlocPhysics/component_system/tlocRigidBodyComponent.h
@@ -6,7 +6,9 @@
 #include <tlocCore/component_system/tlocComponent.h>
 #include <tlocCore/utilities/tlocUtils.h>
 
-#include <Box2D/Dynamics/b2Body.h>
+#include <tlocPhysics/box2d/tlocRigidBodyDef.h>
+#include <tlocPhysics/box2d/tlocRigidBody.h>
+#include <tlocPhysics/box2d/tlocRigidBody.inl>
 
 namespace tloc { namespace physics { namespace component_system {
 
@@ -14,24 +16,22 @@ namespace tloc { namespace physics { namespace component_system {
   {
   public:
     typedef core::component_system::Component_T<RigidBody>  base_type;
-    typedef b2BodyDef                                       rigid_body_def_type;
-    typedef b2Body                                          rigid_body_type;
+    typedef box2d::RigidBodyDef                             rigid_body_def_type;
+    typedef box2d::RigidBody                                rigid_body_value_type;
 
   public:
     RigidBody
       (const rigid_body_def_type& a_box2dRigidBodyDef = rigid_body_def_type());
 
-    rigid_body_type* GetBox2dRigidBody();
+    TLOC_DECL_AND_DEF_GETTERS_DIRECT
+      (rigid_body_value_type, GetRigidBodyValue, m_rigidBody);
 
-    void SetBox2dRigidBody(rigid_body_type* a_box2dRigidBody);
-    void SetBox2dRigidBodyToNull();
-
-    TLOC_DECL_AND_DEF_GETTER(rigid_body_def_type, GetBox2dRigidBodyDef,
-                             m_box2dRigidBodyDef);
+    TLOC_DECL_AND_DEF_GETTERS_DIRECT
+      (rigid_body_def_type, GetRigidBodyDef, m_rigidBodyDef);
 
   private:
-    rigid_body_def_type m_box2dRigidBodyDef;
-    rigid_body_type* m_box2dRigidBody;
+    rigid_body_def_type m_rigidBodyDef;
+    rigid_body_value_type m_rigidBody;
   };
 
 };};};

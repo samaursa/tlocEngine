11555f3e9fbf2f8e7ca425ad35fd8d36c14edd5e
samaursa <samaursa@localhost>
2012-11-25 13:12:00 -0500

    [MIN][ADD] Defined nullptr in tlocBasicTypes | FileIO now has a IsOpen() method (TESTED)

--- src/tlocTests/core/tlocFileIOTest.cpp
+++ src/tlocTests/core/tlocFileIOTest.cpp
@@ -39,20 +39,25 @@ namespace TestingFileIO
     GetPathToCreateFiles(path);
     strcat(path, fileName);
 
+    // Prepare the file that we will be testing
     FILE* m_file = fopen(path, "w");
     REQUIRE(m_file != NULL);
 
     fputs(sentence, m_file);
     fclose(m_file);
 
+    // Start our tests
     io::FileIO_ReadA fileReader(path);
+    CHECK_FALSE(fileReader.IsOpen());
     CHECK(fileReader.Open() == common_error_types::error_success);
+    CHECK(fileReader.IsOpen());
 
     String fileContents;
     fileReader.GetContents(fileContents);
 
     CHECK(fileContents.compare(sentence) == 0);
     CHECK(fileReader.Close() == common_error_types::error_success );
+    CHECK_FALSE(fileReader.IsOpen());
 
     CHECK(fileReader.Delete() == common_error_types::error_success );
   }

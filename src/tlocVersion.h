655284fe00e6acdb77a2725896340ad104efabb8
Matiaus <Matiaus@localhost>
2012-12-04 02:03:35 -0500

    [ADD] Added tlocRigidBodyListener component as well as tlocRigidBodyListener interface

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_3587_1354516712
+#ifndef TLOC_VERSION_0_0_3588_1354604303
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

33bfaf32c52a405cf297a1472229439e77b10c1c
samaursa <samaursa@localhost>
2012-12-09 15:12:40 -0500

    [ADD] Added Camera prefab (UNTESTED) - The reason the prefab is untested is because of issues with our Shader   pipeline, which we are currently fixing.

--- src/tlocPrefab/graphics/tlocCamera.h
+++ src/tlocPrefab/graphics/tlocCamera.h
@@ -6,38 +6,53 @@
 
 namespace tloc { namespace prefab { namespace graphics {
 
-  void CreateCamera(Entity* a_ent,
-                    EntityManager& a_mgr,
-                    core::component_system::ComponentPoolManager& a_poolMgr)
+  core::component_system::Entity*
+    CreateCamera(core::component_system::EntityManager& a_mgr,
+                 core::component_system::ComponentPoolManager& a_poolMgr)
   {
-    TLOC_ASSERT(a_ent, "Entity cannot be NULL");
-
-    using core::component_system;
-    using math::component_system;
-    using math::component_system::components;
-    using graphics::component_system;
-    using graphics::component_system::components;
+    using namespace core::component_system;
+    using namespace math::component_system;
+    using namespace math::component_system::components;
+    using namespace tloc::graphics::component_system;
+    using namespace tloc::graphics::component_system::components;
 
     typedef ComponentPoolManager    pool_mgr;
-    typedef pool_mgr::iterator      comp_pool;
+    typedef pool_mgr::iterator      comp_pool_ptr;
 
-    comp_pool cpool;
+    comp_pool_ptr cpool;
 
     // Get or create the transform pool
     if (a_poolMgr.Exists(transform) == false)
-    { cpool = a_poolMgr.CreateNewPool(transform); }
+    { cpool = a_poolMgr.CreateNewPool<TransformPtr>(transform); }
     else
     { cpool = a_poolMgr.GetPool(transform); }
 
-    comp_pool::iterator itr = cpool.GetNext();
-    itr->GetElement() = new Transform();
-    a_mgr.InsertComponent(a_ent, itr->GetElement()->Expose());
+    typedef tloc::math::component_system::TransformPool  t_pool;
+
+    t_pool* tPool = (*cpool)->GetAs<t_pool>();
+
+    t_pool::iterator itrTransform = tPool->GetNext();
+    itrTransform->GetElement() = new Transform();
 
     // Get or create the projection pool
     if (a_poolMgr.Exists(projection) == false)
-    { cpool = a_poolMgr.CreateNewPool(projection); }
+    { cpool = a_poolMgr.CreateNewPool<ProjectionPtr>(projection); }
     else
     { cpool = a_poolMgr.GetPool(projection); }
+
+    typedef tloc::graphics::component_system::ProjectionPool p_pool;
+
+    p_pool* pPool = (*cpool)->GetAs<p_pool>();
+
+    p_pool::iterator itrProjection = pPool->GetNext();
+    itrProjection->GetElement() = new Projection();
+
+    // Create an entity from the manager and return to user
+    Entity* ent = a_mgr.CreateEntity();
+    a_mgr.InsertComponent(ent, &*(itrTransform->GetElement()) );
+    a_mgr.InsertComponent(ent, &*(itrProjection->GetElement()) );
+
+    return ent;
   }
 
 };};};

fb9e12210459646bee0f85437e9d49c2625d78a3
Matiaus <Matiaus@localhost>
2012-12-13 17:06:34 -0500

    [REM] Removed includes and preprocessor macros defined in tlocEngineTest projects that no longer apply to the projects - Removed includes that were only supposed to be for the input test project - Removed Macros that were used to enable low level asserts (on by default)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_3764_1355435405
+#ifndef TLOC_VERSION_0_0_3766_1355436189
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

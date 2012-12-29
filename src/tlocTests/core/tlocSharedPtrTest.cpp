7258c539c28c2e380002faf85e3bdea74f5e157f
samaursa <samaursa@localhost>
2012-12-29 17:17:01 -0500

    [MIN][REF] Cleaned up SharedPtrTest(s)

--- src/tlocTests/core/tlocSharedPtrTest.cpp
+++ src/tlocTests/core/tlocSharedPtrTest.cpp
@@ -14,6 +14,8 @@ namespace TestingSharedPtr
   using namespace tloc;
   using namespace core;
 
+  using tloc::core::smart_ptr::SharedPtr;
+
   struct SharedStruct
   {
     SharedStruct(tl_int a_value) : m_value(a_value)
@@ -37,10 +39,10 @@ namespace TestingSharedPtr
     SharedStruct::m_numDtors = 0;
   }
 
-  void PassSharedPtr(const smart_ptr::SharedPtr<SharedStruct>& a_other)
+  void PassSharedPtr(const SharedPtr<SharedStruct>& a_other)
   {
     tl_int currCount = a_other.use_count();
-    smart_ptr::SharedPtr<SharedStruct> localPtr = a_other;
+    SharedPtr<SharedStruct> localPtr = a_other;
     CHECK(localPtr.use_count() == currCount + 1);
   }
 
@@ -51,16 +53,16 @@ namespace TestingSharedPtr
   TEST_CASE("core/smart_ptr/shared_ptr", "")
   {
     {
-      smart_ptr::SharedPtr<SharedStruct> sp(nullptr);
+      SharedPtr<SharedStruct> sp(nullptr);
       CHECK_FALSE(sp);
     }
 
     {
-      smart_ptr::SharedPtr<SharedStruct> sp( new SharedStruct(5) );
+      SharedPtr<SharedStruct> sp( new SharedStruct(5) );
       CHECK(sp);
       CHECK_CTOR_DTOR_COUNT(1, 0);
       CHECK(sp.use_count() == 1);
-      smart_ptr::SharedPtr<SharedStruct> sp2 = sp;
+      SharedPtr<SharedStruct> sp2 = sp;
       CHECK_CTOR_DTOR_COUNT(1, 0);
       CHECK(sp.use_count() == 2);
 
@@ -77,18 +79,19 @@ namespace TestingSharedPtr
     CHECK_CTOR_DTOR_COUNT(1, 1);
 
     {
-      smart_ptr::SharedPtr<SharedStruct> sp;
+      SharedPtr<SharedStruct> sp;
       CHECK_FALSE(sp);
       // This SHOULD fire an assertion - because null copy is disabled
-      // smart_ptr::SharedPtr<Shared> sp2 = sp;
+      // SharedPtr<Shared> sp2 = sp;
       CHECK(sp.use_count() == 0);
       //CHECK(sp2.use_count() == 0);
 
     } // Should not crash when being destroyed
 
     {
-      typedef smart_ptr::SharedPtr<SharedStruct,
-              smart_ptr::p_shared_ptr::null_copy::Allow> shared_ptr_type;
+      using tloc::core::smart_ptr::p_shared_ptr::null_copy::Allow;
+
+      typedef SharedPtr<SharedStruct, Allow> shared_ptr_type;
 
       shared_ptr_type sp;
       shared_ptr_type sp2 = sp; // Should NOT fire an assertion
@@ -101,11 +104,11 @@ namespace TestingSharedPtr
     {
       ResetSharedStructStaticVars();
 
-      smart_ptr::SharedPtr<SharedStruct> sp(new SharedStruct(10));
-      smart_ptr::SharedPtr<SharedStruct> sp2 = sp;
+      SharedPtr<SharedStruct> sp(new SharedStruct(10));
+      SharedPtr<SharedStruct> sp2 = sp;
       CHECK(sp.use_count() == 2);
 
-      smart_ptr::SharedPtr<SharedStruct> sp3(new SharedStruct(50));
+      SharedPtr<SharedStruct> sp3(new SharedStruct(50));
       sp2 = sp3;
       CHECK(sp.use_count() == 1);
       CHECK(sp3.use_count() == 2);
@@ -120,8 +123,8 @@ namespace TestingSharedPtr
     {
       ResetSharedStructStaticVars();
 
-      smart_ptr::SharedPtr<SharedStruct> sp(new SharedStruct(10));
-      smart_ptr::SharedPtr<SharedStruct> sp2(new SharedStruct(5));
+      SharedPtr<SharedStruct> sp(new SharedStruct(10));
+      SharedPtr<SharedStruct> sp2(new SharedStruct(5));
 
       CHECK(sp->m_value == 10);
       CHECK(sp2->m_value == 5);
@@ -139,12 +142,12 @@ namespace TestingSharedPtr
   TEST_CASE("core/smart_ptr/shared_ptr/const", "")
   {
     {
-      smart_ptr::SharedPtr<const SharedStruct> sp(nullptr);
+      SharedPtr<const SharedStruct> sp(nullptr);
       CHECK_FALSE(sp);
     }
 
     {
-      smart_ptr::SharedPtr<const SharedStruct> sp(new SharedStruct(10));
+      SharedPtr<const SharedStruct> sp(new SharedStruct(10));
       CHECK(sp);
     }
   }
@@ -154,7 +157,7 @@ namespace TestingSharedPtr
     {
       ResetSharedStructStaticVars();
 
-      smart_ptr::SharedPtr<SharedStruct> sp(new SharedStruct(50));
+      SharedPtr<SharedStruct> sp(new SharedStruct(50));
       CHECK_CTOR_DTOR_COUNT(1, 0);
       sp.reset();
       CHECK_CTOR_DTOR_COUNT(1, 1);
@@ -164,8 +167,8 @@ namespace TestingSharedPtr
     {
       ResetSharedStructStaticVars();
 
-      smart_ptr::SharedPtr<SharedStruct> sp(new SharedStruct(50));
-      smart_ptr::SharedPtr<SharedStruct> sp2(sp);
+      SharedPtr<SharedStruct> sp(new SharedStruct(50));
+      SharedPtr<SharedStruct> sp2(sp);
       sp.reset();
       CHECK_CTOR_DTOR_COUNT(1, 0);
       CHECK(sp2.use_count() == 1);
@@ -174,8 +177,8 @@ namespace TestingSharedPtr
     {
       ResetSharedStructStaticVars();
 
-      smart_ptr::SharedPtr<SharedStruct> sp(new SharedStruct(50));
-      smart_ptr::SharedPtr<SharedStruct> sp3(sp);
+      SharedPtr<SharedStruct> sp(new SharedStruct(50));
+      SharedPtr<SharedStruct> sp3(sp);
       CHECK_CTOR_DTOR_COUNT(1, 0);
       sp.reset(new SharedStruct(10));
       CHECK(sp3);
@@ -184,7 +187,7 @@ namespace TestingSharedPtr
 
       CHECK(sp.use_count() == 1);
 
-      smart_ptr::SharedPtr<SharedStruct> sp2(sp);
+      SharedPtr<SharedStruct> sp2(sp);
       CHECK(sp.use_count() == 2);
       sp.reset();
       CHECK_CTOR_DTOR_COUNT(2, 1);
@@ -198,7 +201,7 @@ namespace TestingSharedPtr
   {
     ResetSharedStructStaticVars();
 
-    typedef smart_ptr::SharedPtr<SharedStruct>      shared_ptr_type;
+    typedef SharedPtr<SharedStruct>      shared_ptr_type;
     typedef T_ContainerType                   shared_array_type;
 
     const tl_int count = 2;
@@ -229,9 +232,9 @@ namespace TestingSharedPtr
 
   TEST_CASE("core/smart_ptr/shared_ptr/with_containers", "")
   {
-    TestContainers<tl_array<smart_ptr::SharedPtr<SharedStruct> >::type>();
-    TestContainers<tl_singly_list<smart_ptr::SharedPtr<SharedStruct> >::type>();
-    TestContainers<tl_doubly_list<smart_ptr::SharedPtr<SharedStruct> >::type>();
+    TestContainers<tl_array<SharedPtr<SharedStruct> >::type>();
+    TestContainers<tl_singly_list<SharedPtr<SharedStruct> >::type>();
+    TestContainers<tl_doubly_list<SharedPtr<SharedStruct> >::type>();
   }
 
   struct MyComponent
@@ -265,7 +268,7 @@ namespace TestingSharedPtr
   {
     const tl_int poolSize = 100;
     {
-      typedef smart_ptr::SharedPtr<MyComponent>             my_comp_ptr;
+      typedef SharedPtr<MyComponent>             my_comp_ptr;
       typedef memory::MemoryPoolIndexed<my_comp_ptr>        my_mem_pool;
       typedef my_mem_pool::iterator                         pool_type;
 
@@ -336,29 +339,29 @@ namespace TestingSharedPtr
   {
     {
       CHECK(base::m_ctorCount == 0);
-      smart_ptr::SharedPtr<base> basePtr(new derived());
+      SharedPtr<base> basePtr(new derived());
       CHECK(base::m_ctorCount == 1);
     }
     CHECK(base::m_dtorCount == 1);
 
     {
-      smart_ptr::SharedPtr<base> basePtr(new derived());
+      SharedPtr<base> basePtr(new derived());
       CHECK(basePtr->m_value == 0);
       basePtr->foo(5);
       CHECK(basePtr->m_value == 5);
 
-      smart_ptr::SharedPtr<derived> derPtr(new derived());
+      SharedPtr<derived> derPtr(new derived());
       derPtr->foo(10);
       basePtr = derPtr;
       CHECK(base::m_dtorCount == 2);
       CHECK(basePtr->m_value == 10);
 
-      smart_ptr::SharedPtr<base> basePtr2(derPtr);
+      SharedPtr<base> basePtr2(derPtr);
 
       // This should fail to compile - so don't uncomment :)
-      //smart_ptr::SharedPtr<derived> d(basePtr2);
+      //SharedPtr<derived> d(basePtr2);
 
-      smart_ptr::SharedPtr<derived> convToDer;
+      SharedPtr<derived> convToDer;
       CHECK(base::m_ctorCount == 3);
       CHECK(base::m_dtorCount == 2);
 
@@ -369,5 +372,9 @@ namespace TestingSharedPtr
     }
     CHECK(base::m_ctorCount == 3);
     CHECK(base::m_dtorCount == 3);
+  }
+
+  TEST_CASE("core/smart_ptr/shared_ptr/global operators", "")
+  {
   }
 }
--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_4081_1356819352
+#ifndef TLOC_VERSION_0_0_4082_1356819413
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

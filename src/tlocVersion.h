cdfae9a55a2c54591b48d25f92039061c3c9aa1a
samaursa <samaursa@localhost>
2012-12-29 17:15:57 -0500

    [OBJ][ADD] Added common_type<T1, T2> type_traits helper that allows us to find the type that is the base of other (TESTED) - tlocTypeTraits         * Includes tlocTypeTraitsCommonType (avoids header clutter)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_4077_1356813119
+#ifndef TLOC_VERSION_0_0_4081_1356819352
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

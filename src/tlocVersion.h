a1d2da7c0532c054a73905a2962bf3585ffd5790
samaursa <samaursa@localhost>
2013-01-03 23:42:28 -0500

    [ADD][OBJ] Working Fan component and FanRenderSystem added (TESTED with Atom&Eve project) - tlocFanRenderSystem         * The system currently calculates each fan's points every time it           processes an entity. Very inefficient, but for now, it works. We will           make it faster when needed.

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_4289_1357203353
+#ifndef TLOC_VERSION_0_0_4311_1357274453
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

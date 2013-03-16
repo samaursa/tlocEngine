177b1edb3232540c18ff329534c198617ecc7454
Matiaus <neil.j.koo@gmail.com>
2013-03-16 04:30:56 -0400

    [FIX][REM] tlocOpenGLView was being offset twice, fixed to be offset only once | removed ability to have smaller than fullscreen resolutions for the iOS window - tlocWindowImplIphone 	* After setting the main view's view controller to root, the view was             being offset twice. With the view controller set to root, the view is             automatically resized to fit the screen, accounting for the titlebar.             We originally accounted for the titlebar as well, causing the view             to offset twice. 	* Smaller than full screen resolutions were removed because the             window was not able to resize to the user specified resolution             once the device was rotated.

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_4851_1362864258
+#ifndef TLOC_VERSION_0_0_4862_1363415781
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

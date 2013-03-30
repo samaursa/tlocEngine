9ceb5bc023abdcac00596e6788e5819f0f8efc8c
samaursa <samaursa@localhost>
2013-03-30 11:57:11 -0400

    [MOD][FIX] Added command line configurations for VS2010 | Fixed error in tlocVersion's CMakeLists - tlocVersion/CMakeLists         * Any variables set in a CMakeLists (even if inherited) are applicable           to the current scope and all its children. The inherited variables           are automatically set back to what they were before (found this           through trial and error - still not 100% sure)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_0_5090_1364600465
+#ifndef TLOC_VERSION_0_0_5096_1364658900
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

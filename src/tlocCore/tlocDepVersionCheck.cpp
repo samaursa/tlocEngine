a866565a59cebadf1655017392de85bd789d169b
samaursa <samaursa@localhost>
2013-04-03 23:05:06 -0400

    [INCR][OBJ] Now looking for libraries in tlocDep's build path, where the build path itself is selected by the user (perhaps should be automated later on) (TESTED on Windows)

--- src/tlocCore/tlocDepVersionCheck.cpp
+++ src/tlocCore/tlocDepVersionCheck.cpp
@@ -1,5 +1,5 @@
 // Compile against the proper dependency version
-#define TLOC_DEP_VERSION_0_0_124_1365042196
+#define TLOC_DEP_VERSION_0_0_126_1365043835
 #include "tlocDepVersion.h"
 
 #include <tlocCore/tlocBase.h>

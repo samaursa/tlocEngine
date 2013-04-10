c0a95b5739784131143e10907b55e37e7415ec4d
samaursa <saadrustam@gmail.com>
2013-04-09 20:43:46 -0400

    [B-FIX][MIN] Build fix on OSX

--- src/tlocGraphics/opengl/tlocError.cpp
+++ src/tlocGraphics/opengl/tlocError.cpp
@@ -155,7 +155,7 @@ namespace tloc { namespace graphics { namespace gl {
     { GLU_OUT_OF_MEMORY, "out of memory"},
     { GLU_INCOMPATIBLE_GL_VERSION, "incompatible gl version"},
     { GLU_INVALID_OPERATION, "invalid operation"},
-    { nullptr, nullptr} /* end of list indicator */
+    { TLOC_NULL, TLOC_NULL} /* end of list indicator */
   };
 
   //------------------------------------------------------------------------

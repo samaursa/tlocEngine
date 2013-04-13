cd76d4c1c1ced13b2176e7d67c4da415ac08a702
samaursa <samaursa@localhost>
2013-04-13 09:17:39 -0400

    [B-FIX] Forgot to include tlocRayTest.cpp

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_5260_1365740264
+#ifndef TLOC_VERSION_0_1_5261_1365859042
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

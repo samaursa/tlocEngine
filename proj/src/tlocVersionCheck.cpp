8119b9fb11d0093a116ca2a23e19b2dc7e6d2a46
samaursa <saadrustam@gmail.com>
2013-04-26 03:33:06 -0400

    [MOD] Improved default project CMakeLists file which now allows creation of test projects as well

--- proj/src/tlocVersionCheck.cpp
+++ proj/src/tlocVersionCheck.cpp
@@ -1,2 +1,2 @@
-#define TLOC_VERSION_0_1_5260_1365740264
+#define TLOC_VERSION_0_1_5507_1366959381
 #include "tlocVersion.h"
--- proj/tlocUserVars.cmake
+++ proj/tlocUserVars.cmake
@@ -1,7 +1,10 @@
 set(USER_EXECUTABLE_PROJECTS "skopworks;")
 #list(APPEND USER_EXECUTABLE_PROJECTS "nextExeProject;")
 
-set(USER_LIBRARY_PROJECTS "skopworksLib")
+set(USER_LIBRARY_PROJECTS "skopworksLib;")
 #list(APPEND USER_LIBRARY_PROJECTS "nextLibraryProject;")
+
+set(USER_TEST_PROJECTS "skopworksTest;")
+#list(APPEND USER_TEST_PROJECTS "nextTestProject;")
 
 set(USER_ASSETS_PATH "${CMAKE_SOURCE_DIR}/src/gameAssetsPath.h")

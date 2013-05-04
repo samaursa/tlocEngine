97d8e9e0a74fb19cff25523764b21c61f7fee8b9
samaursa <saadrustam@gmail.com>
2013-05-04 10:07:07 -0400

    [MIN] Component now can be enabled/disabled

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_5718_1367567394
+#ifndef TLOC_VERSION_0_1_5719_1367629299
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

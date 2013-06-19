182db51d89cd161ba5cfe887660206bdc25a35b8
samaursa <saadrustam@gmail.com>
2013-06-19 11:51:41 -0400

    [OBJ][ADD] Added prefab Mesh() function to create 3D meshes (TESTED with tlocObjLoader sample)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_6334_1371648581
+#ifndef TLOC_VERSION_0_1_6337_1371656854
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

2d29abf86bf974f0b329517bef25f06e629df64d
samaursa <saadrustam@gmail.com>
2013-06-28 20:22:10 -0400

    [REF] Prefab::Quad refactored from a function to an object (TESTED with samples)

--- src/tlocPrefab/graphics/tlocQuad.cpp
+++ src/tlocPrefab/graphics/tlocQuad.cpp
@@ -11,7 +11,6 @@ namespace tloc { namespace prefab { namespace graphics {
   using core_cs::EntityManager;
   using core_cs::ComponentPoolManager;
 
-  using gfx_cs::Quad;
   using gfx_cs::quad_sptr;
 
   using gfx_cs::TextureCoords;
@@ -21,11 +20,19 @@ namespace tloc { namespace prefab { namespace graphics {
   using math_cs::Transform;
   using math_cs::transform_sptr;
 
-  core_cs::Entity*
-    CreateQuad(core_cs::EntityManager& a_mgr,
-               core_cs::ComponentPoolManager& a_poolMgr,
-               const math_t::Rectf32& a_rect,
-               bool a_addTexCoords)
+  Quad::entity_type*
+    Quad::
+    Create()
+  {
+    Entity* ent = m_entMgr->CreateEntity();
+    Add(ent);
+
+    return ent;
+  }
+
+  void
+    Quad::
+    Add(entity_type* a_ent)
   {
     using namespace gfx_cs::components;
     using namespace math_cs::components;
@@ -36,41 +43,40 @@ namespace tloc { namespace prefab { namespace graphics {
     typedef gfx_cs::quad_sptr_pool    quad_pool;
     gfx_cs::quad_sptr_pool_sptr       quadPool;
 
-    if (a_poolMgr.Exists(quad) == false)
-    { quadPool = a_poolMgr.CreateNewPool<quad_sptr>(); }
+    if (m_compPoolMgr->Exists(quad) == false)
+    { quadPool = m_compPoolMgr->CreateNewPool<quad_sptr>(); }
     else
-    { quadPool = a_poolMgr.GetPool<quad_sptr>(); }
+    { quadPool = m_compPoolMgr->GetPool<quad_sptr>(); }
 
     quad_pool::iterator itrQuad = quadPool->GetNext();
-    itrQuad->SetValue(quad_sptr(new Quad(a_rect) ));
+    itrQuad->SetValue(quad_sptr(new gfx_cs::Quad(m_rect) ));
 
     // Create the transform component (and the transform pool if necessary)
     typedef math_cs::transform_f32_sptr_pool  t_pool;
     math_cs::transform_f32_sptr_pool_sptr     tPool;
 
-    if (a_poolMgr.Exists(transform) == false)
-    { tPool = a_poolMgr.CreateNewPool<transform_sptr>(); }
+    if (m_compPoolMgr->Exists(transform) == false)
+    { tPool = m_compPoolMgr->CreateNewPool<transform_sptr>(); }
     else
-    { tPool = a_poolMgr.GetPool<transform_sptr>(); }
+    { tPool = m_compPoolMgr->GetPool<transform_sptr>(); }
 
     t_pool::iterator itrTransform = tPool->GetNext();
     itrTransform->SetValue(transform_sptr(new Transform()) );
 
     // Create an entity from the manager and return to user
-    Entity* ent = a_mgr.CreateEntity();
-    a_mgr.InsertComponent(ent, itrTransform->GetValue().get() );
-    a_mgr.InsertComponent(ent, itrQuad->GetValue().get() );
+    m_entMgr->InsertComponent(a_ent, itrTransform->GetValue().get() );
+    m_entMgr->InsertComponent(a_ent, itrQuad->GetValue().get() );
 
     // Create the texture coords (and the texture coord pool if necessary)
-    if (a_addTexCoords)
+    if (m_texCoords)
     {
       typedef gfx_cs::texture_coords_sptr_pool  tcoord_pool;
       gfx_cs::texture_coords_sptr_pool_sptr     tCoordPool;
 
-      if (a_poolMgr.Exists(texture_coords) == false)
-      { tCoordPool = a_poolMgr.CreateNewPool<texture_coords_sptr>(); }
+      if (m_compPoolMgr->Exists(texture_coords) == false)
+      { tCoordPool = m_compPoolMgr->CreateNewPool<texture_coords_sptr>(); }
       else
-      { tCoordPool = a_poolMgr.GetPool<texture_coords_sptr>(); }
+      { tCoordPool = m_compPoolMgr->GetPool<texture_coords_sptr>(); }
 
       tcoord_pool::iterator itrTCoord = tCoordPool->GetNext();
       texture_coords_sptr tc(new TextureCoords());
@@ -79,10 +85,8 @@ namespace tloc { namespace prefab { namespace graphics {
       tc->AddCoord(math_t::Vec2f32(1.0f, 0.0f));
       tc->AddCoord(math_t::Vec2f32(0.0f, 0.0f));
       itrTCoord->SetValue(tc);
-      a_mgr.InsertComponent(ent, itrTCoord->GetValue().get() );
+      m_entMgr->InsertComponent(a_ent, itrTCoord->GetValue().get() );
     }
-
-    return ent;
   }
 
 };};};

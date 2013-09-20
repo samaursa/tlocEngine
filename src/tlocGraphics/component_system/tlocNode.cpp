405d7ad7226806f187586efc45e5d12695672366
samaursa <saadrustam@gmail.com>
2013-09-19 23:20:51 -0400

    [WIP][ADD] Node component nearly complete

--- src/tlocGraphics/component_system/tlocNode.cpp
+++ src/tlocGraphics/component_system/tlocNode.cpp
@@ -6,11 +6,131 @@
 
 namespace tloc { namespace graphics { namespace component_system {
 
+  // ///////////////////////////////////////////////////////////////////////
+  // Flags
+
+  enum
+  {
+    k_hierarchyUpdate = 0,
+    k_transformUpdate,
+    k_count
+  };
+
+  // ///////////////////////////////////////////////////////////////////////
+  // Node
+
   Node::
     Node()
     : base_type(k_component_type)
+    , m_entity(nullptr)
+    , m_parent(nullptr)
+    , m_level(0)
+    , m_flags(k_count)
   { }
 
   // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Node::
+    Node(entity_ptr_type a_entity)
+    : base_type(k_component_type)
+    , m_entity(a_entity)
+    , m_parent(nullptr)
+    , m_level(0)
+    , m_flags(k_count)
+  { }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Node::
+    AddChild(pointer a_childNode)
+  {
+    TLOC_ASSERT(a_childNode->GetParent() == nullptr, "Child already has a parent");
+
+    a_childNode->SetHierarchyUpdateRequired(true);
+    m_children.push_back(a_childNode);
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Node::
+    RemoveChild(pointer a_childNode)
+  {
+    TLOC_ASSERT(a_childNode->GetParent() == this,
+      "'this' is not the parent of the child");
+
+    node_cont_iterator itr = core::find_all(m_children, a_childNode);
+
+    TLOC_ASSERT(itr != m_children.end(),
+      "Child/parent hierarchy was not added properly in these methods");
+
+    (*itr)->m_parent = nullptr;
+    (*itr)->SetHierarchyUpdateRequired(true);
+
+    m_children.erase(itr);
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  bool
+    Node::
+    HasChild(pointer a_childNode)
+  {
+    node_cont_iterator itr = core::find_all(m_children, a_childNode);
+    return itr != m_children.end();
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Node::node_cont_iterator
+    Node::
+    begin()
+  { return m_children.begin(); }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Node::node_cont_const_iterator
+    Node::
+    begin() const
+  { return m_children.begin(); }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Node::node_cont_iterator
+    Node::
+    end()
+  { return m_children.end(); }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Node::node_cont_const_iterator
+    Node::
+    end() const
+  { return m_children.end(); }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Node::
+    SetLocalTransform(const transform_type& a_transform)
+  {
+    m_localTransform = a_transform;
+    SetTransformUpdateRequired(true);
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Node::
+    SetHierarchyUpdateRequired(bool a_updateRequired)
+  { m_flags[k_hierarchyUpdate] = a_updateRequired; }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Node::
+    SetTransformUpdateRequired(bool a_updateRequired)
+  { m_flags[k_transformUpdate] = a_updateRequired; }
 
 };};};
--- src/tlocGraphics/component_system/tlocNode.h
+++ src/tlocGraphics/component_system/tlocNode.h
@@ -5,14 +5,20 @@
 
 #include <tlocCore/containers/tlocContainers.h>
 #include <tlocCore/smart_ptr/tlocSharedPtr.h>
-#include <tlocCore/component_system/tlocComponentPoolManager.h>
 #include <tlocCore/component_system/tlocComponent.h>
+#include <tlocCore/component_system/tlocComponentPoolManager.h>
+#include <tlocCore/component_system/tlocEntity.h>
+#include <tlocCore/utilities/tlocCheckpoints.h>
+#include <tlocCore/error/tlocError.h>
 
 #include <tlocGraphics/component_system/tlocComponentType.h>
 
 #include <tlocMath/component_system/tlocTransform.h>
 
 namespace tloc { namespace graphics { namespace component_system {
+
+  // ///////////////////////////////////////////////////////////////////////
+  // Node
 
   class Node
     : public core_cs::Component_T<Node, components::node>
@@ -24,29 +30,120 @@ namespace tloc { namespace graphics { namespace component_system {
     typedef this_type*                                    pointer;
     typedef this_type&                                    reference;
 
+    typedef core_err::Error                               error_type;
+
+    typedef const core_cs::Entity*                        entity_ptr_type;
+
     typedef core_conts::tl_array<pointer>::type   node_cont_type;
     typedef node_cont_type::iterator              node_cont_iterator;
     typedef node_cont_type::const_iterator        node_cont_const_iterator;
 
     typedef math_cs::Transformf32                 transform_type;
+    typedef tl_size                               index_type;
 
   public:
     Node();
+    explicit Node(entity_ptr_type a_entity);
 
-    TLOC_DECL_AND_DEF_SETTER(transform_type, SetLocalTransform, m_localTransform);
+    void        AddChild(pointer a_childNode);
+    bool        HasChild(pointer a_childNode);
+    void        RemoveChild(pointer a_childNode);
 
+    node_cont_iterator        begin();
+    node_cont_iterator        end();
+
+    node_cont_const_iterator  begin() const;
+    node_cont_const_iterator  end() const;
+
+    TLOC_DECL_SETTER(transform_type, SetLocalTransform);
     TLOC_DECL_AND_DEF_GETTER_CONST_DIRECT
       (transform_type, GetLocalTransform, m_localTransform);
+    TLOC_DECL_AND_DEF_GETTER(index_type, GetLevel, m_level);
+
+    TLOC_DECL_GETTER(bool, IsHierarchyUpdateRequired);
+    TLOC_DECL_SETTER_BY_VALUE(bool, SetHierarchyUpdateRequired);
+
+    TLOC_DECL_GETTER(bool, IsTransformUpdateRequired);
+    TLOC_DECL_SETTER_BY_VALUE(bool, SetTransformUpdateRequired);
+
+    TLOC_DECL_GETTER(entity_ptr_type, GetEntity);
+    TLOC_DECL_AND_DEF_GETTER(pointer, GetParent, m_parent);
 
   private:
-    this_type*      m_parent;
-    node_cont_type  m_children;
+    TLOC_DECL_AND_DEF_SETTER_BY_VALUE(index_type, DoSetLevel, m_level);
 
-    transform_type  m_localTransform;
+  private:
+    entity_ptr_type         m_entity;
+
+    pointer                 m_parent;
+    node_cont_type          m_children;
+    index_type              m_level;
+
+    transform_type          m_localTransform;
+    core_utils::Checkpoints m_flags;
 
   };
 
-  // -----------------------------------------------------------------------
+  // ///////////////////////////////////////////////////////////////////////
+  // NodeCompare
+
+  template <typename T_NodeType>
+  struct NodeCompareByLevel
+  { TLOC_STATIC_ASSERT_FALSE(T_NodeType, Unsupported_node_type); };
+
+  template <>
+  struct NodeCompareByLevel<Node>
+  {
+  public:
+    typedef Node                value_type;
+
+  public:
+    bool
+      operator()(const value_type& a_first, const value_type& a_second) const
+    {
+      return a_first.GetLevel() < a_second.GetLevel();
+    }
+  };
+
+  template <>
+  struct NodeCompareByLevel<Node*>
+  {
+  public:
+    typedef Node*               value_type;
+
+  public:
+    bool
+      operator()(value_type a_first, value_type a_second) const
+    {
+      return a_first->GetLevel() < a_second->GetLevel();
+    }
+  };
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  // Node compare by entity pointer
+
+  struct NodeCompare
+  {
+  public:
+    typedef Node::entity_ptr_type             entity_ptr_type;
+    typedef Node::pointer                     node_ptr_type;
+
+  public:
+
+    NodeCompare(entity_ptr_type a_entityPtr)
+      : m_entToCompare(a_entityPtr)
+    { }
+
+    bool operator()(node_ptr_type a_other) const
+    {
+      return m_entToCompare == a_other->GetEntity();
+    }
+
+  private:
+    entity_ptr_type m_entToCompare;
+  };
+
+  // ///////////////////////////////////////////////////////////////////////
   // typedefs
 
   TLOC_TYPEDEF_SHARED_PTR(Node, node);

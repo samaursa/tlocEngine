c94a6cdc3b00fc2d60ed57b27307db23b205dc59
samaursa <saadrustam@gmail.com>
2013-09-22 00:38:11 -0400

    [ADD] Entity now inherits from p_entity::Entity_I which can store the Entity name in debug builds

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_7297_1379823915
+#ifndef TLOC_VERSION_0_1_7298_1379824424
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

1e39287026addf7b0011282d5ebbad3fdd14c860
samaursa <saadrustam@gmail.com>
2013-11-18 19:55:11 -0500

    [MAJ][REM] Nedmalloc has been removed and is no longer the default memory allocator for the 2LoC engine (TESTED)

--- src/tlocCore/memory/tlocLinkMe.cpp
+++ src/tlocCore/memory/tlocLinkMe.cpp
@@ -1,12 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 // Link this file when using custom memory allocators
 
-#ifdef TLOC_USING_NED_MALLOC
-
-// nedmalloc defines this without checking
 #ifdef _CRT_SECURE_NO_WARNINGS
   #undef _CRT_SECURE_NO_WARNINGS
 #endif
 # include <tlocCore/memory/tlocAllocators.h>
-# include <3rdParty/nedmalloc/nedmalloc.c>
-#endif

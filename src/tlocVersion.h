b3f7b412025cd5f0980622595b364c05e1040c40
samaursa <saadrustam@gmail.com>
2013-11-23 21:35:18 -0500

    [MIN] Compiled and tested with the latest tlocDep revision (which fixed build error on Xcode/iOS - TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_7921_1385248478
+#ifndef TLOC_VERSION_0_1_7922_1385248479
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

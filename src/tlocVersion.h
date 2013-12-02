ffd418ad5cd5678240c8ac4df1d720876d288448
samaursa <saadrustam@gmail.com>
2013-12-01 19:43:57 -0500

    [ADD] Added necessary source files for our threading library (currently EMPTY)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_8059_1385939839
+#ifndef TLOC_VERSION_0_1_8060_1385944951
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

35e240f93ed0cce017d0f3728a862f4296ebbf72
samaursa <saadrustam@gmail.com>
2013-12-13 11:23:35 -0500

    [WIP][OBJ] Added WeightedAverage tests and fixed syntax errors in the WeightedAverage method (TESTED)

---
+++ src/tlocMath/statistics/tlocStatFunction.inl.h
@@ -0,0 +1,14 @@
+#ifndef _TLOC_MATH_STATISTICS_FUNCTION_INL_
+#define _TLOC_MATH_STATISTICS_FUNCTION_INL_
+
+#ifndef _TLOC_MATH_STATISTICS_FUNCTION_H_
+#error "Must include header before including the inline file"
+#endif
+
+#include "tlocStatFunction.h"
+
+namespace tloc { namespace math { namespace statistics {
+
+};};};
+
+#endif
--- src/tlocTests/math/tlocStatFunctionTest.cpp
+++ src/tlocTests/math/tlocStatFunctionTest.cpp
@@ -31,7 +31,42 @@ namespace TestingStatFunction
                core::MakeBackInsertIterator(dataCont));
 
     Function_T<T_ArrayType*, p_function::Mean> f(&dataCont);
-    CHECK(f.Calculate() == 22);
+    CHECK(f.Calculate() == Approx(22));
+    CHECK(f.GetLastResult() == Approx(22));
+  }
+
+  template <typename T_ArrayType>
+  void TestWeightedAverage()
+  {
+    using namespace math::statistics;
+
+    typedef T_ArrayType                       array_type;
+    typedef typename array_type::value_type   value_type;
+    typedef typename array_type::iterator     itr_type;
+
+    array_type dataCont;
+    dataCont.push_back(0);
+    dataCont.push_back(1);
+
+    Function_T<T_ArrayType*, p_function::WeightedAverage_T
+      <p_function::p_weighted_average::Front> > f(&dataCont);
+
+    CHECK(f.Calculate() == 0.5f);
+
+    dataCont.push_back(2);
+
+    {
+      value_type res = ((0 * 1.0f/3.0f) + (1 * 2.0f/3.0f) + (2 * 3.0f/3.0f)) / 3.0f;
+      CHECK(f.Calculate() == Approx(res) );
+    }
+
+    Function_T<T_ArrayType*, p_function::WeightedAverage_T
+      <p_function::p_weighted_average::Back> > fb(&dataCont);
+
+    {
+      value_type res = ((0 * 3.0f/3.0f) + (1 * 2.0f/3.0f) + (2 * 1.0f/3.0f)) / 3.0f;
+      CHECK(fb.Calculate() == Approx(res) );
+    }
   }
 
   TEST_CASE("math/statistics/Function_T", "")
@@ -39,8 +74,15 @@ namespace TestingStatFunction
     SECTION("mean", "")
     {
       TestMean<core_conts::Array<f32> >();
-      TestMean<core_conts::ArrayFixed<f32, 15> >();
+      TestMean<core_conts::ArrayFixed<f32, 100> >();
       TestMean<core_conts::List<f32> >();
+    }
+
+    SECTION("weighted average", "")
+    {
+      TestWeightedAverage<core_conts::Array<f32> >();
+      TestWeightedAverage<core_conts::ArrayFixed<f32, 100> >();
+      TestWeightedAverage<core_conts::List<f32> >();
     }
   }
 }

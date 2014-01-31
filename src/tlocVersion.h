cc00b220b7e36f6860ed0f8a271a50883199a4a7
samaursa <saadrustam@gmail.com>
2014-01-31 10:23:23 -0500

    [REF][REN] Renamed VirtualPointer source files to VirtualPtr (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_8422_1391181404
+#ifndef TLOC_VERSION_0_1_8424_1391181766
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

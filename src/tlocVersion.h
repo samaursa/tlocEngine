8b0baf4b7cbbdb413a8a44c4fc637622d27a6dbd
samaursa <saadrustam@gmail.com>
2014-03-18 17:44:38 -0400

    [MIN][REM] Removed tlocSmartPtrTracker as the tracker functionality has been moved to (more appropriately) tlocAllocators (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_10126_1395169806
+#ifndef TLOC_VERSION_0_1_10132_1395179015
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

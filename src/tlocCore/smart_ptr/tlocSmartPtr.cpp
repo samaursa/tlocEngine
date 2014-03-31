90fe47b8401b6dfd9c4e489ea03ec39fea1cb330
samaursa <saadrustam@gmail.com>
2014-03-30 23:56:03 -0400

    [B-FIX] Forgot to add tlocSmartPtr.cpp

---
+++ src/tlocCore/smart_ptr/tlocSmartPtr.cpp
@@ -0,0 +1,12 @@
+#include "tlocSmartPtr.h"
+#include "tlocSmartPtr.inl.h"
+
+namespace tloc { namespace core { namespace smart_ptr {
+
+  SmartPtr::SmartPtr()
+  { /* Intentionally Empty */ }
+
+  SmartPtr::~SmartPtr()
+  { /* Intentionally Empty */ }
+  
+};};};

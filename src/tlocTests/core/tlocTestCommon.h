165ce32c816cfb248866c11183b6f743e266be4d
samaursa <saadrustam@gmail.com>
2014-04-15 16:35:27 -0400

    [OBJ][ADD][FIX] Assert modified to support throwing exceptions if desired - mainly for testing (TESTED) | Fixed InitializeAndDestroy destructor bug (see below) (TESTED) - tlocInitializeAndDestroy         * The class was calling the Destroy() method in the destructor which           in turn calls the derived class's destroy method. This produces a           subtle bug where the derived class is destroyed before the base           class (which is InitializeAndDestroy) has a chance to call the           derived DoDestroy() method. This caused a crash on exit in the Font           samples in Release configurations (somehow the Debug configuration           was okay - although it shouldn't be). This is now fixed and the assert           tested with the improved TLOC_ASSERT which can now throw on demand. - tlocAssert         * Has been improved where an assertion can throw an exception instead           of aborting if SetAssertThrow() is called before the assert is fired.           Note that the default behavior is to abort. - tlocTestCommon         * Now has TLOC_TEST_ASSERT and TLOC_TEST_ASSERT_CHECK() macros which           are used together to test code where an assertion is deliberately           triggered. This can be used to ensure assertions are fired properly.

--- src/tlocTests/core/tlocTestCommon.h
+++ src/tlocTests/core/tlocTestCommon.h
@@ -1,5 +1,5 @@
-#ifndef TLOC_TEST_COMMON_H
-#define TLOC_TEST_COMMON_H
+#ifndef TLOC_TEST_CORE_COMMON_H
+#define TLOC_TEST_CORE_COMMON_H
 
 #include <tlocCore/tlocBase.h>
 #if defined (__APPLE__) && defined (__OBJC__)
@@ -11,5 +11,19 @@
 #include <tlocCore/types/tlocTypes.h>
 
 #include "tlocAssetsPath.h"
+
+#define TLOC_TEST_ASSERT\
+  TLOC_ASSERT_THROWS();\
+  bool exceptionThrown = false;\
+  try
+
+#define TLOC_TEST_ASSERT_CHECK()\
+  catch(const tloc::core::assert::exception::Assert&)\
+  {\
+    exceptionThrown = true;\
+  }\
+  CHECK(exceptionThrown);\
+  TLOC_ASSERT_DOES_NOT_THROW()
+
 
 #endif

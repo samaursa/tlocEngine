6e49c80617df2f2564667cd923a84facb8f90cdb
samaursa <saadrustam@gmail.com>
2014-04-17 01:30:47 -0400

    [B-FIX] Build fix due to latest changes in Core (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_10751_1397712108
+#ifndef TLOC_VERSION_0_1_10755_1397712632
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

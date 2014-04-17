e4f8f821131df414ab4d11b27a1dc4e680976648
samaursa <saadrustam@gmail.com>
2014-04-17 10:00:42 -0400

    [WIP][ADD] Added TextRenderSystem_TI (UNTESTED) which will be the base class for both TextRenderSystem and StaticTextRenderSystem (WIP) | Completed the Text component (will be derived from Text_TI)

--- src/tlocGraphics/component_system/tlocText.cpp
+++ src/tlocGraphics/component_system/tlocText.cpp
@@ -1,21 +1,84 @@
-#include "tlocStaticText.h"
+#include "tlocText.h"
 
 namespace tloc { namespace graphics { namespace component_system {
 
-  //// ///////////////////////////////////////////////////////////////////////
-  //// StaticText
+  enum 
+  {
+    k_text_updated,
+    k_alignment_updated,
+    k_count
+  };
 
-  //StaticText::
-  //  StaticText()
-  //  : base_type(k_component_type)
-  //{ }
+  // ///////////////////////////////////////////////////////////////////////
+  // Text
 
-  //// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  Text::
+    Text()
+    : base_type(k_component_type)
+    , m_alignment(alignment::k_align_left)
+    , m_flags(k_count)
+  { }
 
-  //StaticText::
-  //  StaticText(BufferArg a_text)
-  //  : base_type(k_component_type)
-  //  , m_text(a_text)
-  //{ }
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  Text::
+    Text(BufferArgW a_text, align_type a_alignment)
+    : base_type(k_component_type)
+    , m_text(a_text)
+    , m_alignment(a_alignment)
+    , m_flags(k_count)
+  { }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void 
+    Text::
+    Align(align_type a_alignment)
+  { 
+    base_type::SetUpdateRequired(true);
+    m_flags.Mark(k_alignment_updated);
+    m_alignment = a_alignment;
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  void
+    Text::
+    Set(BufferArgW a_text)
+  {
+    base_type::SetUpdateRequired(true);
+    m_flags.Mark(k_text_updated);
+    m_text = a_text;
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  bool
+    Text::
+    TextUpdated() const
+  {
+    return m_flags.ReturnAndUnmark(k_text_updated);
+  }
+
+  // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  bool
+    Text::
+    AlignmentUpdated() const
+  {
+    return m_flags.ReturnAndUnmark(k_alignment_updated);
+  }
 
 };};};
+
+//////////////////////////////////////////////////////////////////////////
+// explicit instantiations
+
+#include <tlocCore/component_system/tlocComponentPoolManager.inl.h>
+
+using namespace tloc::gfx_cs;
+
+// SmartPtr
+TLOC_EXPLICITLY_INSTANTIATE_SHARED_PTR(Text);
+TLOC_EXPLICITLY_INSTANTIATE_UNIQUE_PTR(Text);
+TLOC_EXPLICITLY_INSTANTIATE_COMPONENT_POOL(Text);
--- src/tlocGraphics/component_system/tlocText.h
+++ src/tlocGraphics/component_system/tlocText.h
@@ -8,39 +8,53 @@
 #include <tlocCore/component_system/tlocComponent.h>
 #include <tlocCore/string/tlocString.h>
 #include <tlocCore/memory/tlocBufferArg.h>
+#include <tlocCore/utilities/tlocCheckpoints.h>
 
 #include <tlocGraphics/component_system/tlocComponentType.h>
 #include <tlocGraphics/component_system/tlocTextureCoords.h>
+#include <tlocGraphics/component_system/tlocTextAlignment.h>
 
 #include <tlocMath/types/tlocRectangle.h>
 
 namespace tloc { namespace graphics { namespace component_system {
 
-  //class StaticText
-  //  : public core_cs::Component_T<StaticText, components::static_text>
-  //{
-  //public:
-  //  typedef StaticText                                      this_type;
-  //  typedef Component_T<this_type, components::static_text> base_type;
-  //  typedef f32                                             real_type;
-  //  typedef core_str::String                                str_type;
+  class Text
+    : public core_cs::Component_T<Text, components::text>
+  {
+  public:
+    typedef Text                                            this_type;
+    typedef Component_T<this_type, components::text>        base_type;
+    typedef f32                                             real_type;
+    typedef core_str::StringW                               str_type;
+    typedef ushort                                          font_size_type;
+    typedef alignment::align_type                           align_type;
 
-  //  typedef char8                                       symbol_type;
-  //  typedef core::Pair<symbol_type, math_t::Rectf32_c>  symbol_tcoord_pair_type;
-  //  typedef core_conts::Array<symbol_tcoord_pair_type>  text_cont;
+  public:
+    Text();
+    explicit Text(BufferArgW a_text, 
+                  align_type a_alignment = alignment::k_align_left);
 
-  //public:
-  //  StaticText();
-  //  explicit StaticText(BufferArg a_text);
+    void Align(align_type a_alignment);
 
-  //  TLOC_DECL_AND_DEF_SETTER_BY_VALUE(BufferArg, Set, m_text);
-  //  TLOC_DECL_AND_DEF_GETTER_CONST_DIRECT(str_type, Get, m_text);
+    bool TextUpdated() const;
+    bool AlignmentUpdated() const;
 
-  //private:
-  //  str_type              m_text;
-  //  text_cont             m_symbols;
+    TLOC_DECL_SETTER_BY_VALUE(BufferArgW, Set);
+    TLOC_DECL_AND_DEF_GETTER_CONST_DIRECT(str_type, Get, m_text);
+    TLOC_DECL_AND_DEF_GETTER(align_type, GetAlignment, m_alignment);
 
-  //};
+  private:
+    str_type                        m_text;
+    align_type                      m_alignment;
+    mutable core_utils::Checkpoints m_flags;
+  };
+
+  // -----------------------------------------------------------------------
+  // typedefs
+
+  TLOC_TYPEDEF_ALL_SMART_PTRS(Text, text);
+  TLOC_TYPEDEF_VIRTUAL_STACK_OBJECT(Text, text);
+  TLOC_TYPEDEF_COMPONENT_POOL(Text, text);
 
 };};};
 

387a906518da0fbaeebf7aca2ab41abd65ad0b59
samaursa <saadrustam@gmail.com>
2014-04-21 02:06:01 -0400

    [MIN][B-FIX] No except is not supported in VC++ as of version 2013 (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11039_1398052520
+#ifndef TLOC_VERSION_0_1_11041_1398060194
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

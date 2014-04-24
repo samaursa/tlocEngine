b9cf8f2a67ce5962cefb2547ccfcda7975a9b5ee
samaursa <saadrustam@gmail.com>
2014-04-23 20:44:49 -0400

    [MIN] tlocDep version updated (TESTED on Xcode)

--- src/tlocCore/tlocDepVersionCheck.cpp
+++ src/tlocCore/tlocDepVersionCheck.cpp
@@ -1,5 +1,5 @@
 // Compile against the proper dependency version
-#define TLOC_DEP_VERSION_0_0_156_1397159510 
+#define TLOC_DEP_VERSION_0_0_157_1397159511 
 #include "tlocDepVersion.h"
 
 #include <tlocCore/tlocBase.h>

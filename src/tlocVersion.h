1b44667e8fb9b8054eb49e9a50e475d9074fd0a5
samaursa <saadrustam@gmail.com>
2014-04-27 23:47:25 -0400

    [ADD] Added Window::GetDPI() to get the current device DPI (TESTED on Win32 only - iOS implementation not done yet)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11104_1398565787
+#ifndef TLOC_VERSION_0_1_11110_1398656732
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

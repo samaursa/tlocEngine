a9e20ad5a586fc91f22f2471b2fa77fd36d5445a
samaursa <saadrustam@gmail.com>
2014-05-06 23:00:06 -0400

    [MOD][FIX] Entities disabled by DeactivateHierarchy() before the SceneGraphSystem has initialized do not affect children - FIXED (TESTED with Samples)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11175_1399256885
+#ifndef TLOC_VERSION_0_1_11188_1399431524
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

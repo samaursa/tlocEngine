f7d2b4a1076f278303bf6d235d4e60130033a6c3
samaursa <saadrustam@gmail.com>
2014-05-07 16:35:56 -0400

    [OBJ] Added tlocConsole source for basic console functionality (TESTED) | Refactored Logger to use the new console functions (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11189_1399491362
+#ifndef TLOC_VERSION_0_1_11200_1399494585
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

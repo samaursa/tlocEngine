68fe0861c0dc6c8489ca5527c1f1fab0cd414fe3
samaursa <saadrustam@gmail.com>
2014-05-18 23:06:05 -0400

    [WIP][ADD] Added Cuboid, DynamicText, Fan, Material and Quad tests (TESTED)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11492_1400465635
+#ifndef TLOC_VERSION_0_1_11507_1400468722
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

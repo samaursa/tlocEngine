963d559ad5b39bbe0d2534ea5b77e362ad34a5a7
samaursa <saadrustam@gmail.com>
2014-05-20 12:57:45 -0400

    [MIN][B-FIX] Build-fix for InputTest (VC++ 2013) (TESTED both Debug and Release)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11639_1400567416
+#ifndef TLOC_VERSION_0_1_11642_1400602986
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

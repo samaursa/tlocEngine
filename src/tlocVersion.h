eaec7c1aba260cc5f4118880e49736e55a3b9488
samaursa <saadrustam@gmail.com>
2014-05-20 03:11:52 -0400

    [B-FIX] Build-fix for Xcode (iOS) after recent changes to Input?s return types (TESTED with samples on iOS)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11638_1400567415
+#ifndef TLOC_VERSION_0_1_11639_1400567416
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

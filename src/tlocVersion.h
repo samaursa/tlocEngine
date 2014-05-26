5ca26f93a1ce2fc9df0e0b1f896c72e072a1546b
samaursa <saadrustam@gmail.com>
2014-05-26 13:47:23 -0400

    [MIN] Added smartptr typedefs for DebugTransformRenderSystem

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11865_1401109148
+#ifndef TLOC_VERSION_0_1_11867_1401126082
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

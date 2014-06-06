e10a19df5dfb7ac1e1753105adf051b2e544d916
samaursa <saadrustam@gmail.com>
2014-06-05 22:48:05 -0400

    [MIN] pref_gfx::Mesh was not setting the normals/texcoords flags (TESTED with samples)

--- src/tlocVersion.h
+++ src/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_11919_1401981176
+#ifndef TLOC_VERSION_0_1_11923_1402022808
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

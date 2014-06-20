e9fac94cd93cd3737a5eb1d7070cdde89844a795
samaursa <saadrustam@gmail.com>
2014-06-20 02:03:42 -0400

    [B-FIX] Build fix for iOS (Xcode) wrt tests (COMPILES but NOT yet TESTED) - NOTE: Ensure that when compiling tests all compiler options (RTTI, exceptions)         are enabled. This is because tests are compiled with these options and         if the libraries are not, you will encounter seemingly obscure compiler         linking errors: http://stackoverflow.com/a/4184877/368599

--- src/tlocTests/core/tlocEventManagerTest.cpp
+++ src/tlocTests/core/tlocEventManagerTest.cpp
@@ -170,9 +170,11 @@ namespace TestingEventManager
     CHECK(tracker->GetEventCount(currentEvent) == 2);
 
     // dispatching to selective listeners
+    
+    EventManager::listeners_list exclusion;
+    exclusion.push_back(tracker2.get().get());
     CHECK(tracker2->GetEventCount(currentEvent) == 0);
-    mgr.DispatchNow(EntityEvent(currentEvent, dummyEnt.get()), 
-                    core_ds::MakeTuple(tracker2.get().get()));
+    mgr.DispatchNow(EntityEvent(currentEvent, dummyEnt.get()), exclusion);
     CHECK(tracker2->GetEventCount(currentEvent) == 1);
     CHECK(globalTracker->GetEventCount(currentEvent) == 2);
     CHECK(tracker->GetEventCount(currentEvent) == 2);

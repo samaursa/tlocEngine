d612149e7084383fb82415b8556e7352c8c789ab
samaursa <saadrustam@gmail.com>
2014-09-08 22:41:13 -0400

    [ADD] Matrix3 now has LookAt() functionality (partially TESTED)

--- src/tlocMath/types/tlocMatrix3.cpp
+++ src/tlocMath/types/tlocMatrix3.cpp
@@ -10,3 +10,10 @@ namespace tloc { namespace math { namespace types {
   template class Matrix_T<tloc::f128, 3>;
 
 };};};
+
+using namespace tloc;
+
+#include <tlocCore/types/tlocStrongType.inl.h>
+TLOC_EXPLICITLY_INSTANTIATE_STRONG_TYPE(math_t::Vector_T<f32 TLOC_COMMA 3>, 0);
+TLOC_EXPLICITLY_INSTANTIATE_STRONG_TYPE(math_t::Vector_T<f64 TLOC_COMMA 3>, 0);
+TLOC_EXPLICITLY_INSTANTIATE_STRONG_TYPE(math_t::Vector_T<f128 TLOC_COMMA 3>, 0);

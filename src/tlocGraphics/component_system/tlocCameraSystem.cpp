6e277d992be10268dadedccb1e19186fa8431a74
samaursa <saadrustam@gmail.com>
2014-09-13 19:51:03 -0700

    [ADD] Added Camera::LookAt() function | CameraSystem correctly orients the camera with the camera target (TESTED) | Added COLOR_GREY for Color_T<3, 4>

--- src/tlocGraphics/component_system/tlocCameraSystem.cpp
+++ src/tlocGraphics/component_system/tlocCameraSystem.cpp
@@ -35,24 +35,35 @@ namespace tloc { namespace graphics { namespace component_system {
     using namespace math::component_system::components;
     using namespace graphics::component_system::components;
 
-    typedef Camera::matrix_type         matrix_type;
+    typedef Camera::matrix_type           matrix_type;
+    typedef math_cs::transform_f32_sptr   trans_type;
 
     camera_sptr cam = a_ent->GetComponent<Camera>();
+    trans_type camTrans = a_ent->GetComponent<trans_type::value_type>();
 
     matrix_type viewMat;
     matrix_type m_vpMatrix;
     viewMat.MakeIdentity();
     m_vpMatrix.MakeIdentity();
 
+    // LookAt target
+    if (cam->DoIsTargetUpdated())
+    {
+      const Camera::point_type camPos = camTrans->GetPosition();
+
+      const Camera::point_type newTarget = 
+        (cam->DoGetTarget() - camPos).Inverse() + camPos;
+
+      camTrans->LookAt(newTarget);
+    }
+
     // vMVP, but since we are doing column major, it becomes PVMv
 
-    m_vpMatrix = a_ent->GetComponent
-      <gfx_cs::Camera>()->GetFrustumRef().GetProjectionMatrix();
+    m_vpMatrix = cam->GetFrustumRef().GetProjectionMatrix();
 
     if (a_ent->HasComponent(transform))
     {
-      math_cs::transform_sptr vMat = a_ent->GetComponent<math_cs::Transform>();
-      math_cs::Transform vMatInv = vMat->Invert();
+      math_cs::Transform vMatInv = camTrans->Invert();
       viewMat = vMatInv.GetTransformation().Cast<matrix_type>();
     }
 

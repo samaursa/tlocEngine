fd2c3526ad572d9618883109c538c9644c970cdf
samaursa <saadrustam@gmail.com>
2014-10-08 18:13:33 -0400

    [MIN][FIX] TextureObject wrapping techniques had incorrect enum values (FIXED but UNTESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12302_1411581072
+#ifndef TLOC_VERSION_0_1_12304_1412806375
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

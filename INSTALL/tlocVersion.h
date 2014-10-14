5f39be9ffc08a23009162c9ecefbb4ff09dba5d7
samaursa <saadrustam@gmail.com>
2014-10-14 09:25:09 -0400

    [OBJ][ADD][FIX] Fix the crash in graphics tests, all tests pass | Added more supported types in AttributeVBO (TESTED - not directly tested in ShaderOperator)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12521_1413235957
+#ifndef TLOC_VERSION_0_1_12532_1413293041
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

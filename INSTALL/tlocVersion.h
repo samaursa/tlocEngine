b980f02dcb567e7e78bb09d084f1f8c86dd32eec
samaursa <saadrustam@gmail.com>
2014-10-16 18:52:54 -0400

    [FIX] Adding a scenenode as a chidl to itself now throws an assertion - Issue #83 (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12542_1413496008
+#ifndef TLOC_VERSION_0_1_12550_1413499930
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

f62ed724c95f468b04fc79d28f427557b13c8bcf
samaursa <saadrustam@gmail.com>
2014-10-28 21:23:07 -0400

    [OBJ][ADD] Added GLSL style constructors for Vector_T<3> and Vector_T<4> - Issue #112 (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12781_1414528426
+#ifndef TLOC_VERSION_0_1_12784_1414545484
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

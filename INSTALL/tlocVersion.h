0016da8fe43afdfe029d5d150d2b746b2ff1cd11
samaursa <saadrustam@gmail.com>
2014-10-30 20:38:22 -0400

    [MIN][FIX] Accidently added tlocECS.cpp to the all inclusive header

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12815_1414715576
+#ifndef TLOC_VERSION_0_1_12816_1414715808
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

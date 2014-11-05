6b20e74b333b1c72383ebb00f77ea960f993479b
samaursa <saadrustam@gmail.com>
2014-11-04 20:36:44 -0500

    [MIN] Assertion tests in VirtualPtr were commented out (we didn't have TLOC_TEST_ASSERT before); they are now tested using TLOC_TEST_ASSERT (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_12985_1415140649
+#ifndef TLOC_VERSION_0_1_13004_1415150713
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

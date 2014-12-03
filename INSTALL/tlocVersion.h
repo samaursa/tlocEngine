0e8c828edf69fb5294114efad8891a6ba29ab623
samaursa <saadrustam@gmail.com>
2014-12-02 19:48:40 -0500

    [FIX] WindowImplWin::DoProcessEvent() now checks for window handle before confining mouse and/or making mouse visible (TESTED with FPSCamera sample) | WindowImplWin::m_defaultMouseClip was not initialized - WindowImplWin         * WindowImplWin::DoProcessEvent() is sometimes called with another           window handle which caused the mouse to be confined or invisible when           the game window loses focus. This is now handled with a dedicated           condition.

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_13376_1417562006
+#ifndef TLOC_VERSION_0_1_13383_1417567496
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

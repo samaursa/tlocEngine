8cf7b90c32cbd6ff18dfd7a552f543153a0ab5bb
samaursa <saadrustam@gmail.com>
2014-12-07 21:07:02 -0500

    [MIN] WindowImplIphone now issues window warnings only once per window instance (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_13422_1417994713
+#ifndef TLOC_VERSION_0_1_13423_1417994714
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

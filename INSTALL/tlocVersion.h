b13afe3a7b6da502c3dbf3224bf87b83fce9981f
samaursa <saadrustam@gmail.com>
2015-01-01 19:59:46 -0500

    [MAJ][FIX] The headers were not being copied to the proper include folder in the INSTALL folder (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_13473_1419019796
+#ifndef TLOC_VERSION_0_1_13544_1420150801
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

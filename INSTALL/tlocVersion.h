6c2ad4dbb80760a1baf191e8f1332681069cdab8
samaursa <saadrustam@gmail.com>
2015-01-30 16:11:28 -0500

    [OBJ] RaypickSystem modified to have 3 most common modes (TESTED with ObjLoader sample)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_14125_1422588108
+#ifndef TLOC_VERSION_0_1_14135_1422651820
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

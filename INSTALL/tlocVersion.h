049596592c13dad8d5ac114d23937cf9114d33bb
samaursa <saadrustam@gmail.com>
2015-02-03 13:24:20 -0500

    [WIP][INCR][FIX] Fixed calculation of line size by using advance as per Chris's suggestion | Working on getting horizontal alignment of text to work

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_14142_1422672403
+#ifndef TLOC_VERSION_0_1_14199_1422987752
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

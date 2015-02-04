e0b4b67e7c859e8a8c97fd2a39e65adabb213964
Matiaus <neil.j.koo@gmail.com>
2015-02-03 19:46:53 -0500

    [B-FIX][MIN] Fixed minor syntax errors for clang/LLVM compiler (TESTED)

--- INSTALL/tlocVersion.h
+++ INSTALL/tlocVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_VERSION_0_1_14249_1423006083
+#ifndef TLOC_VERSION_0_1_14250_1423006084
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 

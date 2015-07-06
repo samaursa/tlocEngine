5684d3c5e05cdc0ebbc558dfacddf2b9e52df8bb
samaursa <saadrustam@gmail.com>
2015-07-05 21:54:07 -0400

    [WIP][FIX][OBJ] BinPacker2D (Guillotine) minimally tested (still writing more tests) | Minor fixes in GuillotineBinPack

--- src/tlocMathTests/tlocBinPacker2DTest.cpp
+++ src/tlocMathTests/tlocBinPacker2DTest.cpp
@@ -6,8 +6,22 @@ namespace BinPacker2D
 {
   using namespace tloc;
   using namespace math;
+  using namespace optimize;
 
   TEST_CASE("math/optimize/BinPacker2D", "")
   {
+    typedef Bin::case_type              case_type;
+    typedef Bin::rect_type              rect_type;
+    TLOC_EXPOSE_TYPEDEFS_2(rect_type, width, height);
+
+    bin_vso b(MakeArgs(core_ds::MakeTuple(2, 2)));
+    b->push_back(case_type(rect_type(width(1), height(1)) ));
+    b->push_back(case_type(rect_type(width(1), height(1)) ));
+    b->push_back(case_type(rect_type(width(1), height(1)) ));
+    b->push_back(case_type(rect_type(width(1), height(1)) ));
+
+    optimize::BinPacker2D bp(b.get());
+    auto err = bp.Process<optimize::GuillotineBinPack>();
+    CHECK(err == ErrorSuccess);
   }
 };

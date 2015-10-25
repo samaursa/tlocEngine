d85b55aa1e4272ebe58d1cab354890577937dba9
samaursa <saadrustam@gmail.com>
2015-10-25 00:00:02 -0400

    [OBJ][FIX] Fixed slight error in BinPacker2D and wrote tests (TESTED)

--- src/tlocMathTests/tlocBinPacker2DTest.cpp
+++ src/tlocMathTests/tlocBinPacker2DTest.cpp
@@ -14,14 +14,70 @@ namespace BinPacker2D
     typedef Bin::rect_type              rect_type;
     TLOC_EXPOSE_TYPEDEFS_2(rect_type, width, height);
 
-    bin_vso b(MakeArgs(core_ds::MakeTuple(2, 2)));
-    b->push_back(case_type(rect_type(width(1), height(1)) ));
-    b->push_back(case_type(rect_type(width(1), height(1)) ));
-    b->push_back(case_type(rect_type(width(1), height(1)) ));
-    b->push_back(case_type(rect_type(width(1), height(1)) ));
+    SECTION("Small bin test", "")
+    {
+      bin_vso b(MakeArgs(core_ds::MakeTuple(2, 2)));
+      b->push_back(case_type(rect_type(width(1), height(1)) ));
+      b->push_back(case_type(rect_type(width(1), height(1)) ));
+      b->push_back(case_type(rect_type(width(1), height(1)) ));
+      b->push_back(case_type(rect_type(width(1), height(1)) ));
 
-    optimize::BinPacker2D bp(b.get());
-    auto err = bp.Process<optimize::GuillotineBinPack>();
-    CHECK(err == ErrorSuccess);
+      optimize::BinPacker2D bp(b.get());
+      auto err = bp.Process<optimize::GuillotineBinPack>();
+      CHECK(err == ErrorSuccess);
+      CHECK(b->size_packed() == 4);
+    }
+
+    SECTION("Large bin test", "")
+    {
+      const tl_int binWidth = 10;
+      const tl_int binHeight = 10;
+      bin_vso b(MakeArgs(core_ds::MakeTuple(binWidth, binHeight)));
+
+      SECTION("Few rectangles to pack", "")
+      {
+        b->push_back(case_type(rect_type(width(1), height(1))));
+        b->push_back(case_type(rect_type(width(1), height(1))));
+        b->push_back(case_type(rect_type(width(1), height(1))));
+        b->push_back(case_type(rect_type(width(1), height(1))));
+
+        optimize::BinPacker2D bp(b.get());
+        auto err = bp.Process<optimize::GuillotineBinPack>();
+        CHECK(err == ErrorSuccess);
+        CHECK(b->size_packed() == 4);
+      }
+
+      SECTION("Many rectangles to pack", "")
+      {
+        for (tl_int i = 0; i < binWidth * binHeight; ++i)
+        { b->push_back(case_type(rect_type(width(1), height(1)) )); }
+
+        optimize::BinPacker2D bp(b.get());
+        auto err = bp.Process<optimize::GuillotineBinPack>();
+        CHECK(err == ErrorSuccess);
+        CHECK(b->size_packed() == binWidth * binHeight);
+      }
+
+      SECTION("Different shaped rectangles", "")
+      {
+        b->push_back(case_type(rect_type(width(10), height(2))));
+        b->push_back(case_type(rect_type(width(10), height(2))));
+
+        b->push_back(case_type(rect_type(width(2), height(6))));
+        b->push_back(case_type(rect_type(width(2), height(6))));
+
+        b->push_back(case_type(rect_type(width(6), height(2))));
+        b->push_back(case_type(rect_type(width(6), height(2))));
+
+        b->push_back(case_type(rect_type(width(3), height(2))));
+        b->push_back(case_type(rect_type(width(3), height(2))));
+
+
+        optimize::BinPacker2D bp(b.get());
+        auto err = bp.Process<optimize::GuillotineBinPack>();
+        CHECK(err == ErrorSuccess);
+        CHECK(b->size_packed() == 8);
+      }
+    }
   }
 };

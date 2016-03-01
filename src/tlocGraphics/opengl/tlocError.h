21b4077d69e696058e76651054ad8fe17a92befd
samaursa <saadrustam@gmail.com>
2016-02-29 20:34:45 -0500

    [ADD][FIX] RigidBodySystem was overriding OnComponent* virtual functions which have base class functioanlity (FIXED and TESTED) | Error.h can now be passed to the logger directly

--- src/tlocGraphics/opengl/tlocError.h
+++ src/tlocGraphics/opengl/tlocError.h
@@ -3,6 +3,7 @@
 
 #include <tlocGraphics/tlocGraphicsBase.h>
 
+
 #include <tlocCore/types/tlocBasicTypes.h>
 #include <tlocCore/utilities/tlocUtils.h>
 
@@ -19,21 +20,43 @@ namespace tloc { namespace graphics { namespace gl {
     bool Succeeded();
     bool Failed();
 
-    value_type GetError();
+    value_type GetError() const;
     template <typename T_String>
-    void       GetLastErrorAsString(T_String& a_out);
+    void        GetLastErrorAsString(T_String& a_out) const;
+    const char* GetLastErrorAsString() const;
 
     static void  IgnoreAllErrors(bool a_ignoreAllErrors = true);
 
     static TLOC_DECL_AND_DEF_GETTER_NON_CONST(bool, IsIgnoreAllErrors, 
                                               s_ignoreAllErrors);
   private:
-    value_type          m_lastError;
+    mutable value_type  m_lastError;
     static value_type   s_lastError;
     static const char*  s_lastErrorDesc;
     static bool         s_ignoreAllErrors;
   };
 
 };};};
+
+#include <tlocCore/logging/tlocLog.h>
+
+namespace tloc { namespace core {
+
+  namespace logging {
+
+    template <typename T_Logger, typename T_BuildConfig>
+    const Log_T<T_Logger, T_BuildConfig>&
+      operator << (const Log_T<T_Logger, T_BuildConfig>& a_log,
+                   const gfx_gl::Error& a_err)
+    {
+      a_log << "OpenGL Error: " << a_err.GetLastErrorAsString() << "[" 
+        << a_err.GetError() << "]";
+
+      return a_log;
+    }
+
+  };
+
+};};
 
 #endif
